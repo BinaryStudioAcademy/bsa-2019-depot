63d1549f6e02ae73fa81dc7560fab4bc69985327
Alik <rakhmonov.alibek@binary-studio.com>
2019-08-01 18:50:24 +0300

    add permissions

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-source /home/ubuntu/.bashrc
-source /home/ubuntu/.profile
 cd /home/ubuntu/bsa-2019-depot/client/
 npm install
 cd /home/ubuntu/bsa-2019-depot/server/
--- scripts/app-start.sh
+++ scripts/app-start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-source /home/ubuntu/.bashrc
-source /home/ubuntu/.profile
 cd /home/ubuntu/bsa-2019-depot/server/
 pm2 delete Depot
 pm2 start npm --name "Depot" -- start
