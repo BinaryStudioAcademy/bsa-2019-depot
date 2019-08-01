c0e43fc77dc8ec2cb60013f720166f8b76917e00
Alik <rakhmonov.alibek@binary-studio.com>
2019-08-01 17:00:23 +0300

    Fix EC2 user name

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /home/ec2-user/.bashrc
+source /home/ubuntu/.bashrc
 cd /home/ubuntu/bsa-2019-depot/client/
 npm install
 cd /home/ubuntu/bsa-2019-depot/server/
--- scripts/app-start.sh
+++ scripts/app-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /home/ec2-user/.bashrc
+source /home/ubuntu/.bashrc
 cd /home/ubuntu/bsa-2019-depot/server/
 pm2 delete Depot
 pm2 start npm --name "Depot" -- start
