c34b6b2d5ad0913c93aa0b412764154aeda22501
Alik <rakhmonov.alibek@binary-studio.com>
2019-08-01 16:57:20 +0300

    Try use bashrc

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export NVM_DIR="$HOME/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+source /home/ec2-user/.bashrc
 cd /home/ubuntu/bsa-2019-depot/client/
 npm install
 cd /home/ubuntu/bsa-2019-depot/server/
