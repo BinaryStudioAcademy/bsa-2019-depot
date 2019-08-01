d4047660ca192e7c5a9a1e19cc926eed2572621a
Alik <rakhmonov.alibek@binary-studio.com>
2019-08-01 14:57:39 +0300

    Try fix

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /bin/bash
+export NVM_DIR="$HOME/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 cd /home/ubuntu/bsa-2019-depot/client/
 npm install
 cd /home/ubuntu/bsa-2019-depot/server/
--- scripts/app-start.sh
+++ scripts/app-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /bin/bash
+export NVM_DIR="$HOME/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 cd /home/ubuntu/bsa-2019-depot/server/
 pm2 delete Depot
 pm2 start npm --name "Depot" -- start
