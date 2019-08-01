4353734893053de30e5e73ae97f34ab8eeb99452
Alik <rakhmonov.alibek@binary-studio.com>
2019-08-01 13:53:04 +0300

    Separate scripts

---
+++ scripts/after-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /home/ubuntu/bsa-2019-depot/client/
+npm install
+cd /home/ubuntu/bsa-2019-depot/server/
+npm install
--- scripts/app-start.sh
+++ scripts/app-start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-cd /home/ubuntu/bsa-2019-depot/client/
-npm install
 cd /home/ubuntu/bsa-2019-depot/server/
-npm install
 pm2 delete Depot
 pm2 start npm --name "Depot" -- start
