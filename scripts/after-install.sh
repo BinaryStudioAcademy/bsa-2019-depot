db3dc6763a33b336e36d8d9d3b3ad94f64126fd2
Alik <rakhmonov.alibek@binary-studio.com>
2019-08-01 16:44:09 +0300

    Fix

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export NVM_DIR="$HOME/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+export NVM_DIR="$HOME/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 cd /home/ubuntu/bsa-2019-depot/client/
 npm install
 cd /home/ubuntu/bsa-2019-depot/server/
