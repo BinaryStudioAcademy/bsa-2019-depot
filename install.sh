c77af38ee10b4a9a686b46591ffdc07b65b5d16d
Alik <rakhmonov.alibek@binary-studio.com>
2019-08-01 13:37:38 +0300

    Fix cd command

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-cd /home/ubuntu/bsa-2019-depot/client
+cd /home/ubuntu/bsa-2019-depot/client/
 npm install
-cd /home/ubuntu/bsa-2019-depot/server
+cd /home/ubuntu/bsa-2019-depot/server/
+npm install
 pm2 delete Depot
 pm2 start npm --name "Depot" -- start
