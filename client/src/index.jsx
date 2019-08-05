ece2354a49da7b6b491313f3476f97c8245337c7
Alex Hlushenok <hlushenok.oleksandr@gmail.com>
2019-08-05 13:44:20 +0300

    build: Add SemanticUI to project

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import Home from './scenes/Home';
 
 import './styles/reset.scss';
-// import 'semantic-ui-css/semantic.min.css';
+import 'semantic-ui-css/semantic.min.css';
 import './styles/common.scss';
 
 const target = document.getElementById('root');
