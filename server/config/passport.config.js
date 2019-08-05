8aeb65dab1b1d722f02eded3ba9e01e71dcaaa8b
Aleksandr Kuharenko <sandrk27@gmail.com>
2019-08-05 17:47:23 +0300

    feat: Add registration strategy to passport config

--- server/config/passport.config.js
+++ server/config/passport.config.js
@@ -21,3 +21,25 @@ passport.use(
     }
   })
 );
+
+passport.use(
+  'register',
+  new LocalStrategy(
+    {
+      passReqToCallback: true
+    },
+    async ({ body: { email } }, username, password, done) => {
+      try {
+        const userByEmail = UserRepository.getByEmail(email);
+        if (userByEmail) return done({ status: 401, message: 'This email is already used' }, false);
+
+        const userByUsername = UserRepository.getByUsername(username);
+        if (userByUsername) return done({ status: 401, message: 'This username is already user' }, false);
+
+        return done(null, { email, username, password });
+      } catch (err) {
+        return done(err);
+      }
+    }
+  )
+);
