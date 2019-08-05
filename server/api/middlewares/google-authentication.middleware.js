e041d205e9961245078d8895a398be7771b32ee8
Aleksandr Kuharenko <sandrk27@gmail.com>
2019-08-06 02:41:03 +0300

    refactor: Code style

--- server/api/middlewares/google-authentication.middleware.js
+++ server/api/middlewares/google-authentication.middleware.js
@@ -4,4 +4,7 @@ export const googleMiddleware = passport.authenticate('google', {
   scope: 'https://www.googleapis.com/auth/userinfo.email'
 });
 
-export const googleCallbackMiddleware = passport.authenticate('google', { failureRedirect: '/login', scope: 'https://www.googleapis.com/auth/userinfo.email' });
+export const googleCallbackMiddleware = passport.authenticate('google', {
+  failureRedirect: '/login',
+  scope: 'https://www.googleapis.com/auth/userinfo.email'
+});
