e55ac7591cedd295ff82e7a8740362c563fdf37e
olyakolesnichenko <kolesnichenko.zp@gmail.com>
2019-08-06 13:40:35 +0300

    fix: Fix user migration

--- server/migrations/20190806094300-create-user.js
+++ server/migrations/20190806094300-create-user.js
@@ -1,40 +1,42 @@
-'use strict';
-module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: false,
-        primaryKey: true,
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.literal('gen_random_uuid()')
-      },
-      username: {
-        allowNull: false,
-        type: Sequelize.STRING,
-        unique: true
-      },
-      email: {
-        allowNull: false,
-        type: Sequelize.STRING,
-        unique: true
-      },
-      password: {
-        allowNull: false,
-        type: Sequelize.STRING,
-        unique: true
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Users');
-  }
-};
+export default {
+  up: (queryInterface, Sequelize) => queryInterface.sequelize
+    .query('CREATE EXTENSION IF NOT EXISTS pgcrypto;')
+    .then(() => queryInterface.sequelize.transaction(transaction => Promise.all([
+      queryInterface.createTable('users', {
+        id: {
+          allowNull: false,
+          autoIncrement: false,
+          primaryKey: true,
+          type: Sequelize.UUID,
+          defaultValue: Sequelize.literal('gen_random_uuid()')
+        },
+        username: {
+          allowNull: false,
+          type: Sequelize.STRING,
+          unique: true
+        },
+        email: {
+          allowNull: false,
+          type: Sequelize.STRING,
+          unique: true
+        },
+        password: {
+          allowNull: false,
+          type: Sequelize.STRING,
+          unique: true
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        }
+      })]))),
+  down: queryInterface => queryInterface.sequelize
+    .transaction(transaction => Promise.all([
+      queryInterface.dropTable('users', { transaction }),
+    ]))
+  };
+
