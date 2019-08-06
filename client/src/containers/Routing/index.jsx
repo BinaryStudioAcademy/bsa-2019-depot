5de29aede60f22fd2789272b7af5504b1b35c3ec
denterekhov <den.n.terekhov@gmail.com>
2019-08-06 21:18:44 +0300

    feat: Add Dashboard to routes

--- client/src/containers/Routing/index.jsx
+++ client/src/containers/Routing/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { NotFound, Login } from '../../scenes';
+import Dashboard from '../../containers/Dashboard';
 
 const Routing = () => {
     return (
         <Switch>
             <Route exact path="/login" component={Login} />
+            <Route exact path="/dashboard" component={Dashboard} />
             <Route exact path="*" component={NotFound} />
         </Switch>
     );
