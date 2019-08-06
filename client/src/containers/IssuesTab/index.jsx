bf4610ec2199b72cef1f62c60d10a0e19f034c12
Michael Fridrikhson <fridrihson.1701@gmail.com>
2019-08-06 20:25:41 +0300

    feat: Added row with filter by text and 'New Issue' button

--- client/src/containers/IssuesTab/index.jsx
+++ client/src/containers/IssuesTab/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Dimmer, Loader } from 'semantic-ui-react';
+import { Dimmer, Loader, Icon, Input, Dropdown, Button } from 'semantic-ui-react';
 import { fetchIssues } from '../../routines/routines';
 import IssuesList from '../../components/IssuesList';
 
@@ -36,6 +36,32 @@ class IssuesTab extends React.Component {
         const {
             issuesData: { loading, issues }
         } = this.props;
+
+        // Mock data
+        const openIssues = 20;
+        const closedIssues = 34;
+
+        const sortOptions = [
+            {
+                key: 'createdAt_DESC',
+                text: 'Newest',
+                value: 'createdAt_DESC'
+            },
+            {
+                key: 'createdAt_ASC',
+                text: 'Oldest',
+                value: 'createdAt_ASC'
+            }
+        ];
+
+        const filterOptions = [
+            {
+                key: 0,
+                text: 'Your Issues',
+                value: 'Your Issues'
+            }
+        ];
+
         return (
             <div className={styles.container}>
                 {loading ? (
@@ -43,7 +69,64 @@ class IssuesTab extends React.Component {
                         <Loader />
                     </Dimmer>
                 ) : (
-                    <IssuesList issues={issues} />
+          <>
+            <div className={styles.filterRow}>
+                <Input
+                    label={<Dropdown text="Filters" options={filterOptions} />}
+                    labelPosition="left"
+                    placeholder="Filter by title"
+                />
+                <Button content="New Issue" primary />
+            </div>
+            <div className={styles.issuesContainer}>
+                <div className={styles.issuesHeader}>
+                    <div className="issues-counters">
+                        <span className={styles.openedIssues}>
+                            <Icon name="info" /> {openIssues} Open
+                        </span>
+                        <span>
+                            <Icon name="check" /> {closedIssues} Closed
+                        </span>
+                    </div>
+                    <div className={styles.issueList}></div>
+                    <div className={styles.issueFilters}>
+                        <Dropdown text="Author" icon="filter">
+                            <Dropdown.Menu>
+                                <Input icon="search" iconPosition="left" className="search" placeholder="Filter authors" />
+                                <Dropdown.Menu scrolling>
+                                    {issues
+                                        .map(issue => issue.author)
+                                        .map((author, index) => (
+                                            <Dropdown.Item key={index} text={author.username} value={author.username} />
+                                        ))}
+                                </Dropdown.Menu>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        <Dropdown text="Assignee" icon="filter">
+                            <Dropdown.Menu>
+                                <Input icon="search" iconPosition="left" className="search" placeholder="Filter assignees" />
+                                <Dropdown.Menu scrolling>
+                                    {issues
+                                        .map(issue => issue.assignees)
+                                        .flat()
+                                        .map(assignee => (
+                                            <Dropdown.Item key={assignee.username} text={assignee.username} value={assignee.username} />
+                                        ))}
+                                </Dropdown.Menu>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        <Dropdown text="Sort" icon="filter">
+                            <Dropdown.Menu>
+                                {sortOptions.map(({ key, text, value }) => (
+                                    <Dropdown.Item key={key} text={text} value={value} />
+                                ))}
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
+                </div>
+                <IssuesList issues={issues} />
+            </div>
+          </>
                 )}
             </div>
         );
