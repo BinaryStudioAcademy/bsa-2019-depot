f4bfcb686939ec7f58768004ae3292f678630fcd
olyakolesnichenko <kolesnichenko.zp@gmail.com>
2019-08-06 09:41:34 +0300

    feat: Add /login page to Routing

--- client/src/containers/Routing/index.jsx
+++ client/src/containers/Routing/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Login from '../../components/Login';
 import NotFound from '../../scenes/NotFound';
 
 const Routing = () => {
     return (
         <Switch>
+            <Route exact path="/login" component={Login} />
             <Route exact path="*" component={NotFound} />
         </Switch>
     );
