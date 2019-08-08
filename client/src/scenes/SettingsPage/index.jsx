87bc8234e218ca2bec79f5ff033c73706bf82e24
Vasyl Boyko <vboiko0@gmail.com>
2019-08-08 13:18:45 +0300

    fix: add styles to textarea

--- client/src/scenes/SettingsPage/index.jsx
+++ client/src/scenes/SettingsPage/index.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-no-bind */
 import React from 'react';
-import { Form as SemanticForm, Grid, Menu, Header, Divider, Image, Input, Button, TextArea } from 'semantic-ui-react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Grid, Menu, Header, Divider, Image, Input, Button, TextArea } from 'semantic-ui-react';
+import { Formik, Form, Field } from 'formik';
 import NOTES from './notes';
 
 const SettingsPage = () => (
@@ -18,49 +18,49 @@ const SettingsPage = () => (
             <Grid>
                 <Grid.Column width={11}>
                     <Formik
-                        initialValues={{ name: 'Vasyl Boyko', email: 'vboiko0@gmail.com' }}
-                        validate={values => {
-                            let errors = {};
-                            if (!values.email) {
-                                errors.email = 'Required';
-                            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-                                errors.email = 'Invalid email address';
-                            }
-                            return errors;
+                        initialValues={{
+                            name: 'Vasyl Boyko',
+                            email: 'vboiko0@gmail.com',
+                            bio: 'Just a bit more about myself',
+                            url: 'https://depothub.xyz/vb-oiko',
+                            company: 'Binary Academy 2019',
+                            location: 'Kiyv',
+                            imgUrl:
+                'https://avatars3.githubusercontent.com/u/43011715?s=400&u=ad60df6df50e7d47ba97c26709508502feb4f342&v=4'
                         }}
-                        // onSubmit={(values, { setSubmitting }) => {
-                        //     setTimeout(() => {
-                        //         alert(JSON.stringify(values, null, 2));
-                        //         setSubmitting(false);
-                        //     }, 400);
-                        // }}
+                        onSubmit={(values, { setSubmitting }) => {
+                            setTimeout(() => {
+                                alert(JSON.stringify(values, null, 2));
+                                setSubmitting(false);
+                            }, 400);
+                        }}
                     >
                         {({ isSubmitting }) => (
-                            <Form>
+                            <Form className="ui form">
                                 <Header as="h4">Name</Header>
                                 <Field type="text" name="name" render={({ field }) => <Input fluid {...field} />} />
-                                <ErrorMessage name="name" component="div" />
                                 <div>{NOTES.name}</div>
 
                                 <Header as="h4">Public email</Header>
                                 <Field type="email" name="email" render={({ field }) => <Input fluid {...field} />} />
-                                <ErrorMessage name="email" component="div" />
                                 <div>{NOTES.email}</div>
 
                                 <Header as="h4">Bio</Header>
                                 <Field
                                     name="bio"
-                                    render={() => (
-                                        <SemanticForm>
-                                            <TextArea rows="3" placeholder="Tell us a little bit about yourself" />
-                                        </SemanticForm>
+                                    render={({ field }) => (
+                                        <TextArea
+                                            {...field}
+                                            rows="3"
+                                            placeholder="Tell us a little bit about yourself"
+                                            style={{ maxHeight: 200 }}
+                                        />
                                     )}
                                 />
                                 <div>{NOTES.bio}</div>
 
                                 <Header as="h4">URL</Header>
                                 <Field type="text" name="url" render={({ field }) => <Input fluid {...field} />} />
-                                <ErrorMessage name="url" component="div" />
 
                                 <Header as="h4">Company</Header>
                                 <Field type="text" name="company" render={({ field }) => <Input fluid {...field} />} />
