359fab1a3550d4ae0430e00226b287ca4f2bf3ca
Olga Gnatenko <olgagnat13@gmail.com>
2019-08-08 06:15:57 +0300

    fix: Remove stubs, fix frontend route

--- client/src/containers/Routing/index.jsx
+++ client/src/containers/Routing/index.jsx
@@ -9,7 +9,7 @@ import { NotFound, Login, MainPage, Signup } from '../../scenes';
 const Routing = () => {
     return (
         <Switch>
-            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/registration" component={Signup} />
             <Route exact path="/" component={MainPage} />
             <Route exact path="/login" component={Login} />
             <PrivateRoute exact path="/" component={MainPage} />
