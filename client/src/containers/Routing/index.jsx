1ff3ad77e4497898268d61af49bf110c1318ef7c
Michael Fridrikhson <fridrihson.1701@gmail.com>
2019-08-08 14:14:22 +0300

    feat: Added styling for 'Set status'

--- client/src/containers/Routing/index.jsx
+++ client/src/containers/Routing/index.jsx
@@ -5,12 +5,14 @@ import { Route, Switch } from 'react-router-dom';
 
 import PrivateRoute from '../PrivateRoute';
 import { NotFound, Login, MainPage, Signup, Dashboard } from '../../scenes';
+import Header from '../Header';
 
 const Routing = () => {
     return (
         <Switch>
             <Route exact path="/registration" component={Signup} />
             <Route exact path="/login" component={Login} />
+            <Route exact path="/header" component={Header} />
             <PrivateRoute exact path="/" component={MainPage} />
             <PrivateRoute exact path="/dashboard" component={Dashboard} />
             <Route exact path="*" component={NotFound} />
