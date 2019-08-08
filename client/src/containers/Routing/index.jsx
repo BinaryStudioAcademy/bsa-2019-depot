3ee52c95732117802269bf386ea0b99690254ba7
Vasyl Boyko <vboiko0@gmail.com>
2019-08-08 22:52:37 +0300

    feat: Add route for settings page

--- client/src/containers/Routing/index.jsx
+++ client/src/containers/Routing/index.jsx
@@ -3,13 +3,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import PrivateRoute from '../PrivateRoute';
-import { NotFound, Login, MainPage } from '../../scenes';
+import { NotFound, Login, MainPage, SettingsPage } from '../../scenes';
 
 const Routing = () => {
     return (
         <Switch>
             <Route exact path="/" component={MainPage} />
             <Route exact path="/login" component={Login} />
+            <Route exact path="/settings" component={SettingsPage} />
             <PrivateRoute exact path="/" component={MainPage} />
             <PrivateRoute exact path="/dashboard" component={MainPage} />
             <Route exact path="*" component={NotFound} />
--- client/src/scenes/index.js
+++ client/src/scenes/index.js
@@ -2,5 +2,6 @@ import Login from './Login/index';
 import Home from './Home/index';
 import NotFound from './NotFound/index';
 import MainPage from './MainPage/index';
+import SettingsPage from './SettingsPage/index';
 
-export { Login, Home, NotFound, MainPage };
+export { Login, Home, NotFound, MainPage, SettingsPage };
