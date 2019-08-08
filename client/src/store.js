eca1a28e182806c48b865f70667e0c790021577e
olyakolesnichenko <kolesnichenko.zp@gmail.com>
2019-08-08 19:01:50 +0300

    refactor: Clean code

--- client/src/store.js
+++ client/src/store.js
@@ -3,30 +3,10 @@ import createSagaMiddleware from 'redux-saga';
 import rootReducer from './reducers/index';
 import rootSaga from './sagas/index';
 
-import { createLogger } from 'redux-logger'; //TODO need to remove
-
-const logger = createLogger({
-    duration: true,
-    collapsed: true,
-    colors: {
-        title: () => '#139BFE',
-        prevstate: () => '#1C5FAF',
-        action: () => '#149945',
-        nextState: () => '#A47104',
-        error: () => '#FF0005'
-    }
-}); //TODO need to remove
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const sagaMiddleware = createSagaMiddleware();
 
-///
-
-const middlewares = [sagaMiddleware, logger];
-export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middlewares)));
-/////TODO need to remove
-
-//export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(sagaMiddleware)));
+export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(sagaMiddleware)));
 
 sagaMiddleware.run(rootSaga);
