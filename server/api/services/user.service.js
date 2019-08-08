ff3b31f0dfe47e9dfac11e2702ac4b3e51393731
Andrew Minikh <andreoven@gmail.com>
2019-08-08 11:20:25 +0300

    feat: Add routes to reset password

--- server/api/services/user.service.js
+++ server/api/services/user.service.js
@@ -1,4 +1,8 @@
+const jwt = require('jsonwebtoken');
 const UserRepository = require('../../data/repositories/user.repository');
+const { sendTokenEmail } = require('../../helpers/email.helper');
+
+const secret = process.env.SECRET_KEY;
 
 const setUsername = async ({ id, username }) => {
   const data = UserRepository.setUsernameById(id, username);
@@ -21,8 +25,34 @@ const checkEmailExists = async ({ email }) => {
   };
 };
 
+const sendForgetPasswordEmail = async ({ email }) => {
+  const isExist = await checkEmailExists({ email });
+  if (!isExist.emailExists) {
+    return { failure: 'Email is not exist' };
+  }
+  const user = await UserRepository.getByEmail(email);
+  const token = jwt.sign({ data: user.email }, secret, { expiresIn: '1h' });
+  sendTokenEmail(email, token);
+  return { success: 'Email with password reset link was sent' };
+};
+
+const resetPassword = async ({ token, password }) => {
+  let result;
+  await jwt.verify(token, secret, async (err, authorizedData) => {
+    if (err) {
+      result = { failure: 'Incorrect token' };
+    } else {
+      await UserRepository.setUserPassword(authorizedData.data, password);
+      result = { success: 'Password updated' };
+    }
+  });
+  return result;
+};
+
 module.exports = {
   setUsername,
   checkUsernameExists,
-  checkEmailExists
+  checkEmailExists,
+  sendForgetPasswordEmail,
+  resetPassword
 };
