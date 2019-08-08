225f7e09b0d0cdd8d2dcb0691f7af9d0e8cd70f4
olyakolesnichenko <kolesnichenko.zp@gmail.com>
2019-08-08 18:54:01 +0300

    refactor: Refactor Forgot page ui

--- client/src/scenes/Forgot/index.jsx
+++ client/src/scenes/Forgot/index.jsx
@@ -9,25 +9,27 @@ import validator from 'validator';
 import { Grid, Header, Form, Button, Segment, Message } from 'semantic-ui-react';
 import { forgotActions } from './actions';
 
+import './styles.module.scss';
+
 class Forgot extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      isLoading: false,
-      isEmailValid: true
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            isLoading: false,
+            isEmailValid: true
+        };
+    }
 
   validateEmail = () => {
-    const { email } = this.state;
-    const isEmailValid = !validator.isEmpty(email);
-    this.setState({ isEmailValid });
-    return isEmailValid;
+      const { email } = this.state;
+      const isEmailValid = !validator.isEmpty(email);
+      this.setState({ isEmailValid });
+      return isEmailValid;
   };
 
   emailChangeHandler = createHandler(ev => {
-    this.emailChanged(ev.target.value);
+      this.emailChanged(ev.target.value);
   });
 
   emailChanged = email => this.setState({ email, isEmailValid: true });
@@ -35,88 +37,89 @@ class Forgot extends Component {
   validateForm = () => [this.validateEmail()].every(Boolean);
 
   handleClickForgot = async () => {
-    const { isLoading, email } = this.state;
-    const { actions } = this.props;
-    const valid = this.validateForm();
-    if (!valid || isLoading) {
-      return;
-    }
-    this.setState({ isLoading: true });
-    try {
-      await actions.forgotAsync({ email });
-      this.setState({ isLoading: false });
-    } catch (error) {
-      this.setState({ isLoading: false });
-    }
+      const { isLoading, email } = this.state;
+      const { actions } = this.props;
+      const valid = this.validateForm();
+      if (!valid || isLoading) {
+          return;
+      }
+      this.setState({ isLoading: true });
+      try {
+          await actions.forgotAsync({ email });
+          this.setState({ isLoading: false });
+      } catch (error) {
+          this.setState({ isLoading: false });
+      }
   };
 
   render() {
-    const { isLoading, isEmailValid } = this.state;
-    const { isAuthorized, message, emailNotExist, emailSend } = this.props;
-    const succsessMessage = emailSend ? <Message color="teal">{message}</Message> : null;
-    const failureMessage = emailNotExist ? <Message color="red">{message}</Message> : null;
-    return !isAuthorized ? (
-      <Grid textAlign="center" verticalAlign="middle" className="fill">
-        <Grid.Column style={{ maxWidth: 450 }}>
-          <Header as="h2" color="blue" textAlign="center">
-            Forgot password?
-          </Header>
-          {succsessMessage}
-          <Form name="forgotForm" size="large" onSubmit={this.handleClickForgot} disabled={emailSend}>
-            <Segment>
-              <Form.Input
-                fluid
-                disabled={emailSend}
-                placeholder="Email"
-                type="email"
-                error={!isEmailValid || emailNotExist}
-                onChange={this.emailChangeHandler()}
-                onBlur={this.validateEmail}
-              />
-              <Button type="submit" color="blue" fluid size="large" loading={isLoading} disabled={emailSend}>
-                Reset password
-              </Button>
-            </Segment>
-            {failureMessage}
-          </Form>
-        </Grid.Column>
-      </Grid>
-    ) : (
-      <Redirect to="/" />
-    );
+      const { isLoading, isEmailValid } = this.state;
+      const { isAuthorized, message, emailNotExist, emailSend } = this.props;
+      const succsessMessage = emailSend ? <Message color="teal">{message}</Message> : null;
+      const failureMessage = emailNotExist ? <Message color="red">{message}</Message> : null;
+      return !isAuthorized ? (
+          <Grid textAlign="center" style={{ height: '100vh' }} verticalAlign="middle" className="forgot-grid fill">
+              <Grid.Column style={{ maxWidth: 400 }}>
+                  <Header as="h2" color="black" textAlign="center" className="forgot-header">
+            Reset your password
+                  </Header>
+                  {succsessMessage}
+                  <Form name="forgotForm" size="large" onSubmit={this.handleClickForgot} disabled={emailSend}>
+                      <Segment>
+                          <Form.Input
+                              fluid
+                              label="Enter your email address and we will send you a link to reset your password."
+                              disabled={emailSend}
+                              placeholder="Enter your email address"
+                              type="email"
+                              error={!isEmailValid || emailNotExist}
+                              onChange={this.emailChangeHandler()}
+                              onBlur={this.validateEmail}
+                          />
+                          <Button type="submit" color="green" fluid size="large" loading={isLoading} disabled={emailSend}>
+                Send password reset email
+                          </Button>
+                      </Segment>
+                      {failureMessage}
+                  </Form>
+              </Grid.Column>
+          </Grid>
+      ) : (
+          <Redirect to="/" />
+      );
   }
 }
 Forgot.propTypes = {
-  isAuthorized: PropTypes.bool,
-  message: PropTypes.string,
-  emailNotExist: PropTypes.bool,
-  emailSend: PropTypes.bool,
-  actions: PropTypes.object,
-  isLoading: PropTypes.bool
+    isAuthorized: PropTypes.bool,
+    message: PropTypes.string,
+    emailNotExist: PropTypes.bool,
+    emailSend: PropTypes.bool,
+    actions: PropTypes.object,
+    isLoading: PropTypes.bool
 };
 
 Forgot.defaultProps = {
-  isAuthorized: false,
-  isLoading: false,
-  message: '',
-  emailNotExist: false,
-  emailSend: false
+    isAuthorized: false,
+    isLoading: false,
+    message: '',
+    emailNotExist: false,
+    emailSend: false
 };
 
 const mapStateToProps = state => ({
-  isAuthorized: state.auth.isAuthorized,
-  message: state.forgot.message,
-  emailNotExist: state.forgot.emailNotExist,
-  emailSend: state.forgot.emailSend
+    isAuthorized: state.auth.isAuthorized,
+    message: state.forgot.message,
+    emailNotExist: state.forgot.emailNotExist,
+    emailSend: state.forgot.emailSend
 });
 
 const mapDispatchToProps = dispatch => {
-  return {
-    actions: bindActionCreators({ ...forgotActions }, dispatch)
-  };
+    return {
+        actions: bindActionCreators({ ...forgotActions }, dispatch)
+    };
 };
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(Forgot);
--- client/src/scenes/Reset/index.jsx
+++ client/src/scenes/Reset/index.jsx
@@ -9,47 +9,49 @@ import validator from 'validator';
 import { Grid, Header, Form, Button, Segment, Message } from 'semantic-ui-react';
 import { resetActions } from './actions';
 
+import './styles.module.scss';
+
 class Reset extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      password: '',
-      repeatPassword: '',
-      isLoading: false,
-      isPasswordValid: true,
-      isRepeatPasswordValid: true,
-      isConfirmedPassword: true,
-      redirect: false
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            password: '',
+            repeatPassword: '',
+            isLoading: false,
+            isPasswordValid: true,
+            isRepeatPasswordValid: true,
+            isConfirmedPassword: true,
+            redirect: false
+        };
+    }
 
   validatePassword = () => {
-    const { password } = this.state;
-    const isPasswordValid = !validator.isEmpty(password);
-    this.setState({ isPasswordValid });
-    return isPasswordValid;
+      const { password } = this.state;
+      const isPasswordValid = !validator.isEmpty(password);
+      this.setState({ isPasswordValid });
+      return isPasswordValid;
   };
 
   validateRepeatPassword = () => {
-    const { repeatPassword } = this.state;
-    const isRepeatPasswordValid = !validator.isEmpty(repeatPassword);
-    this.setState({ isRepeatPasswordValid });
-    return isRepeatPasswordValid;
+      const { repeatPassword } = this.state;
+      const isRepeatPasswordValid = !validator.isEmpty(repeatPassword);
+      this.setState({ isRepeatPasswordValid });
+      return isRepeatPasswordValid;
   };
 
   confirmPassword = () => {
-    const { password, repeatPassword } = this.state;
-    const isConfirmedPassword = password === repeatPassword;
-    this.setState({ isConfirmedPassword });
-    return isConfirmedPassword;
+      const { password, repeatPassword } = this.state;
+      const isConfirmedPassword = password === repeatPassword;
+      this.setState({ isConfirmedPassword });
+      return isConfirmedPassword;
   };
 
   passwordChangeHandler = createHandler(ev => {
-    this.passwordChanged(ev.target.value);
+      this.passwordChanged(ev.target.value);
   });
 
   repeatPasswordChangeHandler = createHandler(ev => {
-    this.repeatPasswordChanged(ev.target.value);
+      this.repeatPasswordChanged(ev.target.value);
   });
 
   passwordChanged = password => this.setState({ password, isPasswordValid: true });
@@ -59,106 +61,109 @@ class Reset extends Component {
   validateForm = () => [this.validatePassword(), this.validateRepeatPassword(), this.confirmPassword()].every(Boolean);
 
   handleClickReset = async () => {
-    const { isLoading, password, repeatPassword } = this.state;
-    const { actions } = this.props;
-    const valid = this.validateForm();
+      const { isLoading, password } = this.state;
+      const { actions } = this.props;
+      const valid = this.validateForm();
 
-    if (!valid || isLoading) {
-      return;
-    }
-    this.setState({ isLoading: true });
-    try {
-      await actions.resetAsync({ password: password, token: this.props.location.pathname.substring(7) });
-      this.setState({ isLoading: false });
-    } catch (error) {
-      this.setState({ isLoading: false, password: '', repeatPassword: '' });
-    }
+      if (!valid || isLoading) {
+          return;
+      }
+      this.setState({ isLoading: true });
+      try {
+          await actions.resetAsync({ password: password, token: this.props.location.pathname.substring(7) });
+          this.setState({ isLoading: false });
+      } catch (error) {
+          this.setState({ isLoading: false, password: '', repeatPassword: '' });
+      }
   };
 
   renderRedirect = succsessMessage => {
-    if (succsessMessage !== null) {
-      setTimeout(() => this.setState({ redirect: true }), 3000);
-    }
+      if (succsessMessage !== null) {
+          setTimeout(() => this.setState({ redirect: true }), 3000);
+      }
   };
 
   render() {
-    const { isLoading, isPasswordValid, isRepeatPasswordValid, isConfirmedPassword } = this.state;
-    const { isAuthorized, message, passwordNotReset, passwordReset } = this.props;
-    const succsessMessage = passwordReset ? <Message color="teal">{message}</Message> : null;
-    const failureMessage = passwordNotReset ? <Message color="red">{message}</Message> : null;
-    this.renderRedirect(succsessMessage);
-    if (this.state.redirect === true) return <Redirect to="/login" />;
-    return !isAuthorized ? (
-      <Grid textAlign="center" verticalAlign="middle" className="fill">
-        <Grid.Column style={{ maxWidth: 450 }}>
-          <Header as="h2" color="blue" textAlign="center">
+      const { isLoading, isPasswordValid, isRepeatPasswordValid, isConfirmedPassword } = this.state;
+      const { isAuthorized, message, passwordNotReset, passwordReset } = this.props;
+      const succsessMessage = passwordReset ? <Message color="teal">{message}</Message> : null;
+      const failureMessage = passwordNotReset ? <Message color="red">{message}</Message> : null;
+      this.renderRedirect(succsessMessage);
+      if (this.state.redirect === true) return <Redirect to="/login" />;
+      return !isAuthorized ? (
+          <Grid textAlign="center" style={{ height: '100vh' }} verticalAlign="middle" className="reset-grid fill">
+              <Grid.Column style={{ maxWidth: 400 }}>
+                  <Header as="h2" color="black" textAlign="center" className="reset-header">
             Reset password
-          </Header>
-          {succsessMessage}
-          <Form name="resetForm" size="large" onSubmit={this.handleClickReset}>
-            <Segment>
-              <Form.Input
-                fluid
-                disabled={passwordReset}
-                placeholder="New password"
-                type="password"
-                error={!isPasswordValid}
-                onChange={this.passwordChangeHandler()}
-                onBlur={this.validatePassword}
-              />
-              <Form.Input
-                fluid
-                disabled={passwordReset}
-                placeholder="Repeat password"
-                type="password"
-                error={!isRepeatPasswordValid && !isConfirmedPassword}
-                onChange={this.repeatPasswordChangeHandler()}
-                onBlur={this.validateRepeatPassword}
-              />
-              <Button type="submit" color="blue" fluid size="large" loading={isLoading} disabled={passwordReset}>
+                  </Header>
+                  {succsessMessage}
+                  <Form name="resetForm" size="large" onSubmit={this.handleClickReset}>
+                      <Segment>
+                          <Form.Input
+                              fluid
+                              label="New password"
+                              disabled={passwordReset}
+                              placeholder="New password"
+                              type="password"
+                              error={!isPasswordValid}
+                              onChange={this.passwordChangeHandler()}
+                              onBlur={this.validatePassword}
+                          />
+                          <Form.Input
+                              fluid
+                              label="Repeat password"
+                              disabled={passwordReset}
+                              placeholder="Repeat password"
+                              type="password"
+                              error={!isRepeatPasswordValid && !isConfirmedPassword}
+                              onChange={this.repeatPasswordChangeHandler()}
+                              onBlur={this.validateRepeatPassword}
+                          />
+                          <Button type="submit" color="green" fluid size="large" loading={isLoading} disabled={passwordReset}>
                 Set new password
-              </Button>
-            </Segment>
-            {failureMessage}
-          </Form>
-        </Grid.Column>
-      </Grid>
-    ) : (
-      <Redirect to="/" />
-    );
+                          </Button>
+                      </Segment>
+                      {failureMessage}
+                  </Form>
+              </Grid.Column>
+          </Grid>
+      ) : (
+          <Redirect to="/" />
+      );
   }
 }
 Reset.propTypes = {
-  isAuthorized: PropTypes.bool,
-  actions: PropTypes.object,
-  isLoading: PropTypes.bool,
-  message: PropTypes.string,
-  passwordNotReset: PropTypes.bool,
-  passwordReset: PropTypes.bool
+    isAuthorized: PropTypes.bool,
+    actions: PropTypes.object,
+    isLoading: PropTypes.bool,
+    message: PropTypes.string,
+    passwordNotReset: PropTypes.bool,
+    passwordReset: PropTypes.bool,
+    location: PropTypes.object
 };
 
 Reset.defaultProps = {
-  isAuthorized: false,
-  isLoading: false,
-  message: '',
-  passwordNotReset: false,
-  passwordReset: false
+    isAuthorized: false,
+    isLoading: false,
+    message: '',
+    passwordNotReset: false,
+    passwordReset: false
 };
 
 const mapStateToProps = state => ({
-  isAuthorized: state.auth.isAuthorized,
-  message: state.reset.message,
-  passwordNotReset: state.reset.passwordNotReset,
-  passwordReset: state.reset.passwordReset
+    isAuthorized: state.auth.isAuthorized,
+    message: state.reset.message,
+    passwordNotReset: state.reset.passwordNotReset,
+    passwordReset: state.reset.passwordReset
 });
 
 const mapDispatchToProps = dispatch => {
-  return {
-    actions: bindActionCreators({ ...resetActions }, dispatch)
-  };
+    return {
+        actions: bindActionCreators({ ...resetActions }, dispatch)
+    };
 };
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(Reset);
