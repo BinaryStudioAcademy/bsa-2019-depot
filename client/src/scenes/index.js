76c145d5de5405321efc6ed29a2d73cd5d5bebb5
Nick <niiickolay@gmail.com>
2019-08-09 18:16:25 +0300

    refactor: Rename component

--- client/src/scenes/index.js
+++ client/src/scenes/index.js
@@ -5,5 +5,17 @@ import MainPage from './MainPage/index';
 import Dashboard from './Dashboard/index';
 import Signup from './Signup/index';
 import CodeTab from './CodeTab/index';
+import CreateRepository from './CreateRepository';
+import EmptyRepoPage from './EmptyRepoPage';
 
-export { Login, Home, NotFound, MainPage, Signup, Dashboard, CodeTab };
+export {
+    Login,
+    Home,
+    NotFound,
+    MainPage,
+    Signup,
+    Dashboard,
+    CodeTab,
+    CreateRepository,
+    EmptyRepoPage
+};
