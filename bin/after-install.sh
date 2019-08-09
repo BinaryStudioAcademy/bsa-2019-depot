34cda8eaa1b6b3c71ada5204de4534d8cd3d31c2
Alik <rakhmonov.alibek@binary-studio.com>
2019-08-09 19:10:54 +0300

    try to run install on Travis

--- bin/after-install.sh
+++ bin/after-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd /home/ubuntu/bsa-2019-depot/client/
-npm install
-npm run build
-cd /home/ubuntu/bsa-2019-depot/server/
-npm install
+# cd /home/ubuntu/bsa-2019-depot/client/
+# npm install
+# npm run build
+# cd /home/ubuntu/bsa-2019-depot/server/
+# npm install
