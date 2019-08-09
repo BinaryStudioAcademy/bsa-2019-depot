b682a25086e559b4372ed22c564549639df9d7f3
Alik <rakhmonov.alibek@binary-studio.com>
2019-08-09 18:20:27 +0300

    Show msg if server started

--- server/server.js
+++ server/server.js
@@ -26,4 +26,7 @@ const port = process.env.APP_PORT || 3000;
 
 app.use(errorHandlerMiddleware);
 
-app.listen(port);
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server listening on port ${port}!`);
+});
