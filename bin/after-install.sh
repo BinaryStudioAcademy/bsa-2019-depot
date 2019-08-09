7534c3f44aa1af9f629435745bb6bfef7bd85458
Alik <rakhmonov.alibek@binary-studio.com>
2019-08-09 19:59:49 +0300

    show message if connected to the db

--- bin/after-install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# cd /home/ubuntu/bsa-2019-depot/client/
-# npm install
-# npm run build
-# cd /home/ubuntu/bsa-2019-depot/server/
-# npm install
--- server/data/db/connection.js
+++ server/data/db/connection.js
@@ -3,8 +3,14 @@ const config = require('../../config/db.config');
 
 const sequelize = new Sequelize(config);
 
-sequelize.authenticate().catch((err) => {
-  console.error('Unable to connect to the database:', err);
-});
+sequelize
+  .authenticate()
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 module.exports = sequelize;
