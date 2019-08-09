58a6f1d3f93e96fed6e0aca7514bc0107c56a7d6
Alik <rakhmonov.alibek@binary-studio.com>
2019-08-09 15:14:01 +0300

    fix

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-whoami
 cd /home/ubuntu/bsa-2019-depot/client/
-/home/ubuntu/.nvm/versions/node/v10.16.2/bin/npm install
+npm install
 cd /home/ubuntu/bsa-2019-depot/server/
-/home/ubuntu/.nvm/versions/node/v10.16.2/bin/npm install
+npm install
