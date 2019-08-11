1adc6741b5fc8cb9480133e6a0ff6ac53f7681db
Vasyl Boyko <vboiko0@gmail.com>
2019-08-11 14:27:22 +0300

    refactor: to class component

--- client/src/scenes/SettingsProfile/index.jsx
+++ client/src/scenes/SettingsProfile/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
     Grid,
     Menu,
@@ -22,147 +22,119 @@ import { updateUserSettings } from '../../routines/routines';
 import styles from './styles.module.scss';
 import avatarPlaceholder from './avatar-placeholder.svg';
 
-const renderField = ({ field }) => <Input fluid {...field} />;
+class SettingsProfile extends Component {
+  renderField = ({ field }) => <Input fluid {...field} />;
 
-const renderDisabledField = ({ field }) => <Input disabled fluid {...field} />;
+  renderDisabledField = ({ field }) => <Input disabled fluid {...field} />;
 
-const renderTextArea = ({ field }) => (
-    <TextArea {...field} rows="3" placeholder="Tell us a little bit about yourself" className={styles.textarea} />
-);
+  renderTextArea = ({ field }) => (
+      <TextArea {...field} rows="3" placeholder="Tell us a little bit about yourself" className={styles.textarea} />
+  );
 
-const onSubmit = (values, { setSubmitting }) => {
-    // console.warn(updateUserSettings);
-    // console.warn(values);
+  onSubmit = (values, { setSubmitting }) => {
+      const { id, bio, company, location, name, imgUrl, url } = values;
+      const payload = { id, settings: { bio, company, location, name, imgUrl, url } };
+      this.props.updateUserSettings(payload);
+  };
 
-    updateUserSettings({ values });
-    setSubmitting(false);
+  onUploadPhoto = ev => {
+      console.warn('Upload photo!');
+  };
 
-    // setTimeout(() => {
-    //     alert(JSON.stringify(values, null, 2));
-    //     setSubmitting(false);
-    // }, 400);
-};
+  onRemovePhoto = ev => {
+      console.warn('Remove photo!');
+  };
 
-const onUploadPhoto = ev => {
-    console.warn('Upload photo!');
-};
-
-const onRemovePhoto = ev => {
-    console.warn('Remove photo!');
-};
-
-const imgEditBtn = (
+  imgEditBtn = (
     <Button secondary compact size="tiny">
         <Icon name="edit" />
-    Edit
+      Edit
     </Button>
-);
+  );
 
-const SettingsProfile = props => (
-    <div>
-        <PageHeader />
-        <Divider hidden />
-        <Grid container>
-            <Grid.Column computer={4} tablet={8} mobile={16}>
-                <Menu vertical>
-                    <Menu.Item header>Personal settings</Menu.Item>
-                    <Menu.Item>Profile</Menu.Item>
-                </Menu>
-            </Grid.Column>
-            <Grid.Column computer={12} tablet={16} mobile={16}>
-                <Header as="h2">Public profile</Header>
-                <Divider />
-                <Grid>
-                    <Grid.Column computer={11} tablet={16} mobile={16}>
-                        <Formik initialValues={props.currentUser} onSubmit={onSubmit}>
-                            {({ isSubmitting }) => (
-                                <Form className="ui form">
-                                    <Header as="h4">Name</Header>
-                                    <Field type="text" name="name" render={renderField} />
-                                    <div>{NOTES.name}</div>
+  render() {
+      return (
+          <div>
+              <PageHeader />
+              <Divider hidden />
+              <Grid container>
+                  <Grid.Column computer={4} tablet={8} mobile={16}>
+                      <Menu vertical>
+                          <Menu.Item header>Personal settings</Menu.Item>
+                          <Menu.Item>Profile</Menu.Item>
+                      </Menu>
+                  </Grid.Column>
+                  <Grid.Column computer={12} tablet={16} mobile={16}>
+                      <Header as="h2">Public profile</Header>
+                      <Divider />
+                      <Grid>
+                          <Grid.Column computer={11} tablet={16} mobile={16}>
+                              <Formik initialValues={this.props.currentUser} onSubmit={this.onSubmit}>
+                                  {({ isSubmitting }) => (
+                                      <Form className="ui form">
+                                          <Header as="h4">Name</Header>
+                                          <Field type="text" name="name" render={this.renderField} />
+                                          <div>{NOTES.name}</div>
 
-                                    <Header as="h4">Public email</Header>
-                                    <Field type="email" name="email" render={renderDisabledField} />
-                                    <div>{NOTES.email}</div>
+                                          <Header as="h4">Public email</Header>
+                                          <Field type="email" name="email" render={this.renderDisabledField} />
+                                          <div>{NOTES.email}</div>
 
-                                    <Header as="h4">Bio</Header>
-                                    <Field name="bio" render={renderTextArea} />
-                                    <div>{NOTES.bio}</div>
+                                          <Header as="h4">Bio</Header>
+                                          <Field name="bio" render={this.renderTextArea} />
+                                          <div>{NOTES.bio}</div>
 
-                                    <Header as="h4">URL</Header>
-                                    <Field type="text" name="url" render={renderField} />
+                                          <Header as="h4">URL</Header>
+                                          <Field type="text" name="url" render={this.renderField} />
 
-                                    <Header as="h4">Company</Header>
-                                    <Field type="text" name="company" render={renderField} />
-                                    <div>{NOTES.company}</div>
+                                          <Header as="h4">Company</Header>
+                                          <Field type="text" name="company" render={this.renderField} />
+                                          <div>{NOTES.company}</div>
 
-                                    <Divider />
+                                          <Divider />
 
-                                    <Header as="h4">Location</Header>
-                                    <Field type="text" name="location" render={renderField} />
+                                          <Header as="h4">Location</Header>
+                                          <Field type="text" name="location" render={this.renderField} />
 
-                                    <Divider hidden />
-                                    <div>{NOTES.disclaimer}</div>
-                                    <Divider hidden />
+                                          <Divider hidden />
+                                          <div>{NOTES.disclaimer}</div>
+                                          <Divider hidden />
 
-                                    <Button color="green" type="submit" disabled={isSubmitting}>
-                    Update profile
-                                    </Button>
-                                </Form>
-                            )}
-                        </Formik>
-                    </Grid.Column>
-                    <Grid.Column computer={5} tablet={8} mobile={16}>
-                        <Segment basic className={styles.no_padding}>
-                            <Header as="h4">Profile picture</Header>
-                            <Image src={props.currentUser.imgUrl || avatarPlaceholder} fluid rounded></Image>
-                            <Dropdown trigger={imgEditBtn} pointing="top left" icon={null} className={styles.img_edit_btn}>
-                                <Dropdown.Menu className={styles.dropdown}>
-                                    <Dropdown.Item onClick={onUploadPhoto}>Upload a photo...</Dropdown.Item>
-                                    <Dropdown.Item onClick={onRemovePhoto}>Remove photo</Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown>
-                        </Segment>
-                    </Grid.Column>
-                </Grid>
-            </Grid.Column>
-        </Grid>
-    </div>
-);
-
-renderField.propTypes = {
-    field: PropTypes.object
-};
-
-renderDisabledField.propTypes = {
-    field: PropTypes.object
-};
-
-renderTextArea.propTypes = {
-    field: PropTypes.object
-};
+                                          <Button color="green" type="submit" disabled={this.props.currentUser.loading}>
+                        Update profile
+                                          </Button>
+                                      </Form>
+                                  )}
+                              </Formik>
+                          </Grid.Column>
+                          <Grid.Column computer={5} tablet={8} mobile={16}>
+                              <Segment basic className={styles.no_padding}>
+                                  <Header as="h4">Profile picture</Header>
+                                  <Image src={this.props.currentUser.imgUrl || avatarPlaceholder} fluid rounded></Image>
+                                  <Dropdown trigger={this.imgEditBtn} pointing="top left" icon={null} className={styles.img_edit_btn}>
+                                      <Dropdown.Menu className={styles.dropdown}>
+                                          <Dropdown.Item onClick={this.onUploadPhoto}>Upload a photo...</Dropdown.Item>
+                                          <Dropdown.Item onClick={this.onRemovePhoto}>Remove photo</Dropdown.Item>
+                                      </Dropdown.Menu>
+                                  </Dropdown>
+                              </Segment>
+                          </Grid.Column>
+                      </Grid>
+                  </Grid.Column>
+              </Grid>
+          </div>
+      );
+  }
+}
 
 SettingsProfile.propTypes = {
     currentUser: PropTypes.object,
-    loading: PropTypes.bool,
-    error: PropTypes.object
-};
-
-SettingsProfile.defaultProps = {
-    currentUser: {
-        name: '',
-        email: '',
-        bio: '',
-        url: '',
-        company: '',
-        location: '',
-        imgUrl: ''
-    },
-    loading: false,
-    error: null
+    updateUserSettings: PropTypes.func
 };
 
 const mapStateToProps = state => {
+    console.warn(state.profile);
+
     return state.profile;
 };
 
