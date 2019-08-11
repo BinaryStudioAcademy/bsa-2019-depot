6a2d2ecf70d17f25deb60da40dfac260066042c7
Vasyl Boyko <vboiko0@gmail.com>
2019-08-11 23:11:52 +0300

    feat: Add modal

--- client/src/components/ForkButton/index.jsx
+++ client/src/components/ForkButton/index.jsx
@@ -1,38 +1,56 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Button, Label, Icon, Modal, Header, List, Image, Divider } from 'semantic-ui-react';
+import { Button, Label, Icon, Modal, Header, List, Image } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 
+import styles from './styles.module.scss';
+
 class ForkButton extends Component {
-    render(props) {
-        return (
-            <Modal
-                trigger={
-                    <Button size="small" as="div" compact labelPosition="right">
-                        <Button size="small" compact>
-                            <Icon name="fork" />
+  state = { modalOpen: false };
+
+  handleOpen = () => this.setState({ modalOpen: true });
+  handleClose = () => this.setState({ modalOpen: false });
+
+  render(props) {
+      return (
+          <Modal
+              closeIcon
+              open={this.state.modalOpen}
+              onClose={this.handleClose}
+              size="small"
+              trigger={
+                  <Button size="small" as="div" compact labelPosition="right" onClick={this.handleOpen}>
+                      <Button size="small" compact>
+                          <Icon name="fork" />
               Fork
-                        </Button>
-                        <Label as="a" basic pointing="left">
-                            {this.props.forkCount}
-                        </Label>
-                    </Button>
-                }
-            >
-                <Modal.Header>Fork {this.props.repoName}</Modal.Header>
-                <Modal.Content>
-                    <Header>Where should we fork {this.props.repoName}?</Header>
-                    <List>
-                        <List.Item>
-                            <Image avatar src={null} />
-                            <Header as="div">this.props.currentUser.username</Header>
-                        </List.Item>
-                    </List>
-                    <Divider />
-                </Modal.Content>
-            </Modal>
-        );
-    }
+                      </Button>
+                      <Label as="a" basic pointing="left">
+                          {this.props.forkCount}
+                      </Label>
+                  </Button>
+              }
+          >
+              <Modal.Header>Fork {this.props.repoName}</Modal.Header>
+              <Modal.Content>
+                  <Header>Where should we fork {this.props.repoName}?</Header>
+                  <List>
+                      <List.Item className={styles.cursor_pointer}>
+                          <Image avatar src="https://i.pravatar.cc/300" />
+                          <Label basic size="big" className={styles.username}>
+                              {this.props.currentUser.username}
+                          </Label>
+                      </List.Item>
+                  </List>
+              </Modal.Content>
+              <Modal.Actions>
+                  <Button onClick={this.handleClose}>Cancel</Button>
+                  <Button primary onClick={this.handleClose}>
+            Fork
+                  </Button>
+              </Modal.Actions>
+          </Modal>
+      );
+  }
 }
 
 ForkButton.propTypes = {
