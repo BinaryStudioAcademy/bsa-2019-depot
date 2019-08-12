f56cdb130e5d427bfd61f66a1e010e901613e653
Olga Gnatenko <olgagnat13@gmail.com>
2019-08-12 14:09:47 +0300

    fix: Correct login redirect

--- client/src/scenes/index.js
+++ client/src/scenes/index.js
@@ -5,8 +5,7 @@ import Home from './Home/index';
 import NotFound from './NotFound/index';
 import MainPage from './MainPage/index';
 import Signup from './Signup/index';
-import GoogleCallback from './GoogleCallback/index';
 import Dashboard from './Dashboard/index';
 import CodeTab from './CodeTab/index';
 
-export { Login, Forgot, Reset, Home, NotFound, MainPage, Signup, GoogleCallback, Dashboard, CodeTab };
+export { Login, Forgot, Reset, Home, NotFound, MainPage, Signup, Dashboard, CodeTab };
