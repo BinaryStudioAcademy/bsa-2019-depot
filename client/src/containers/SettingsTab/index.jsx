7115f06a8faee1b4ef441a7409e091464cfc8810
Aleksandr Kuharenko <sandrk27@gmail.com>
2019-08-12 12:25:25 +0300

    refactor: Use updated eslint

--- client/src/containers/SettingsTab/index.jsx
+++ client/src/containers/SettingsTab/index.jsx
@@ -7,143 +7,143 @@ import { renameRepo, deleteRepo } from './actions';
 import styles from './styles.module.scss';
 
 const renderDangerField = (header, description, buttonName, clickHandler) => (
-    <Message className={styles.dangerField}>
-        <Grid>
-            <Grid.Column width={12}>
-                <Header as="h6">{header}</Header>
-                <p>{description}</p>
-            </Grid.Column>
-            <Grid.Column width={4}>
-                <Button type="button" className={styles.dangerButton} fluid onClick={clickHandler}>
-                    {buttonName}
-                </Button>
-            </Grid.Column>
-        </Grid>
-    </Message>
+  <Message className={styles.dangerField}>
+    <Grid>
+      <Grid.Column width={12}>
+        <Header as="h6">{header}</Header>
+        <p>{description}</p>
+      </Grid.Column>
+      <Grid.Column width={4}>
+        <Button type="button" className={styles.dangerButton} fluid onClick={clickHandler}>
+          {buttonName}
+        </Button>
+      </Grid.Column>
+    </Grid>
+  </Message>
 );
 
 class RepoSettings extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        const {
-            match: {
-                params: { owner, repository }
-            }
-        } = this.props;
-        this.oldName = repository;
+    const {
+      match: {
+        params: { owner, repository }
+      }
+    } = this.props;
+    this.oldName = repository;
 
-        this.state = {
-            name: repository,
-            owner
-        };
-    }
+    this.state = {
+      name: repository,
+      owner
+    };
+  }
 
-    componentDidMount() {
-        const { owner, name } = this.state;
-        this.props.fetchRepoSettings({
-            owner,
-            name
-        });
-    }
+  componentDidMount() {
+    const { owner, name } = this.state;
+    this.props.fetchRepoSettings({
+      owner,
+      name
+    });
+  }
 
   onClickDelete = () => {
-      const { owner, name } = this.state;
-      this.props.deleteRepo({
-          owner,
-          name
-      });
+    const { owner, name } = this.state;
+    this.props.deleteRepo({
+      owner,
+      name
+    });
   };
 
   handleChangeRepoName = event => {
-      this.setState({
-          name: event.target.value
-      });
+    this.setState({
+      name: event.target.value
+    });
   };
 
   onClickRename = () => {
-      const { name, owner } = this.state;
-      const { oldName } = this;
+    const { name, owner } = this.state;
+    const { oldName } = this;
 
-      this.props.renameRepo({
-          owner,
-          oldName,
-          name
-      });
-      this.props.history.push(`/${owner}/${name}/settings`);
-      window.location.reload();
+    this.props.renameRepo({
+      owner,
+      oldName,
+      name
+    });
+    this.props.history.push(`/${owner}/${name}/settings`);
+    window.location.reload();
   };
 
   render() {
-      const { loading } = this.props.repoSettingsData;
-      return !loading ? (
-          <Grid container stackable>
-              <Grid.Column width={4}>
-                  <Menu vertical>
-                      <Menu.Item>Options</Menu.Item>
-                  </Menu>
-              </Grid.Column>
-              <Grid.Column width={12}>
-                  <Header as="h2">Settings</Header>
-                  <Divider />
+    const { loading } = this.props.repoSettingsData;
+    return !loading ? (
+      <Grid container stackable>
+        <Grid.Column width={4}>
+          <Menu vertical>
+            <Menu.Item>Options</Menu.Item>
+          </Menu>
+        </Grid.Column>
+        <Grid.Column width={12}>
+          <Header as="h2">Settings</Header>
+          <Divider />
 
-                  <Header as="h4">Repository name</Header>
-                  <Input className={styles.text_input} defaultValue={this.state.name} onChange={this.handleChangeRepoName} />
-                  <Button className={styles.button_rename} type="button" onClick={this.onClickRename}>
+          <Header as="h4">Repository name</Header>
+          <Input className={styles.text_input} defaultValue={this.state.name} onChange={this.handleChangeRepoName} />
+          <Button className={styles.button_rename} type="button" onClick={this.onClickRename}>
             Rename
-                  </Button>
+          </Button>
 
-                  <Header as="h2">Danger Zone</Header>
-                  <div className={styles.dangerZone}>
-                      {renderDangerField(
-                          'Delete this repository',
-                          'Once you delete a repository, there is no going back. Please be certain.',
-                          'Delete this repository',
-                          this.onClickDelete
-                      )}
-                  </div>
-              </Grid.Column>
-          </Grid>
-      ) : (
-          <Loader />
-      );
+          <Header as="h2">Danger Zone</Header>
+          <div className={styles.dangerZone}>
+            {renderDangerField(
+              'Delete this repository',
+              'Once you delete a repository, there is no going back. Please be certain.',
+              'Delete this repository',
+              this.onClickDelete
+            )}
+          </div>
+        </Grid.Column>
+      </Grid>
+    ) : (
+      <Loader />
+    );
   }
 }
 
 RepoSettings.propTypes = {
-    repoSettingsData: PropTypes.exact({
-        loading: PropTypes.bool.isRequired,
-        error: PropTypes.string,
-        settings: PropTypes.exact({
-            name: PropTypes.string,
-            owner: PropTypes.string,
-            id: PropTypes.string,
-            isPublic: PropTypes.bool
-        })
-    }).isRequired,
-    match: PropTypes.exact({
-        params: PropTypes.object.isRequired,
-        isExact: PropTypes.bool.isRequired,
-        path: PropTypes.string.isRequired,
-        url: PropTypes.string.isRequired
-    }).isRequired,
-    history: PropTypes.object,
-    fetchRepoSettings: PropTypes.func.isRequired,
-    renameRepo: PropTypes.func.isRequired,
-    deleteRepo: PropTypes.func.isRequired
+  repoSettingsData: PropTypes.exact({
+    loading: PropTypes.bool.isRequired,
+    error: PropTypes.string,
+    settings: PropTypes.exact({
+      name: PropTypes.string,
+      owner: PropTypes.string,
+      id: PropTypes.string,
+      isPublic: PropTypes.bool
+    })
+  }).isRequired,
+  match: PropTypes.exact({
+    params: PropTypes.object.isRequired,
+    isExact: PropTypes.bool.isRequired,
+    path: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired
+  }).isRequired,
+  history: PropTypes.object,
+  fetchRepoSettings: PropTypes.func.isRequired,
+  renameRepo: PropTypes.func.isRequired,
+  deleteRepo: PropTypes.func.isRequired
 };
 
 const mapStateToProps = ({ repoSettingsData }) => ({
-    repoSettingsData
+  repoSettingsData
 });
 
 const mapDispatchToProps = {
-    fetchRepoSettings,
-    renameRepo,
-    deleteRepo
+  fetchRepoSettings,
+  renameRepo,
+  deleteRepo
 };
 
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(RepoSettings);
--- client/src/containers/SettingsTab/sagas.js
+++ client/src/containers/SettingsTab/sagas.js
@@ -4,52 +4,52 @@ import * as types from './types';
 import { fetchRepoSettings } from '../../routines/routines';
 
 function* settingsRequest({ payload: { owner, name } }) {
-    try {
-        yield put(fetchRepoSettings.request());
+  try {
+    yield put(fetchRepoSettings.request());
 
-        const response = yield call(repoSettingsService.getSettings, { owner, name });
-        yield put(fetchRepoSettings.success(response));
-    } catch (error) {
-        yield put(fetchRepoSettings.failure(error.message));
-    } finally {
-        yield put(fetchRepoSettings.fulfill());
-    }
+    const response = yield call(repoSettingsService.getSettings, { owner, name });
+    yield put(fetchRepoSettings.success(response));
+  } catch (error) {
+    yield put(fetchRepoSettings.failure(error.message));
+  } finally {
+    yield put(fetchRepoSettings.fulfill());
+  }
 }
 
 function* watchSettingsRequest() {
-    yield takeEvery(fetchRepoSettings.TRIGGER, settingsRequest);
+  yield takeEvery(fetchRepoSettings.TRIGGER, settingsRequest);
 }
 
 export function* renameRepo(action) {
-    try {
-        const { name, owner } = action.payload;
-        yield call(repoSettingsService.renameRepo, { ...action.payload });
-        const response = yield call(repoSettingsService.getSettings, { owner, name });
-        yield put(fetchRepoSettings.success(response));
-    } catch (error) {
-        yield put(fetchRepoSettings.request(error.message));
-    }
+  try {
+    const { name, owner } = action.payload;
+    yield call(repoSettingsService.renameRepo, { ...action.payload });
+    const response = yield call(repoSettingsService.getSettings, { owner, name });
+    yield put(fetchRepoSettings.success(response));
+  } catch (error) {
+    yield put(fetchRepoSettings.request(error.message));
+  }
 }
 
 function* watchRenameRepo() {
-    yield takeEvery(types.RENAME_REPO, renameRepo);
+  yield takeEvery(types.RENAME_REPO, renameRepo);
 }
 
 export function* deleteRepo(action) {
-    try {
-        const { name, owner } = action.payload;
-        yield call(repoSettingsService.deleteRepo, { ...action.payload });
-        const response = yield call(repoSettingsService.getSettings, { owner, name });
-        yield put(fetchRepoSettings.success(response));
-    } catch (error) {
-        yield put(fetchRepoSettings.request(error.message));
-    }
+  try {
+    const { name, owner } = action.payload;
+    yield call(repoSettingsService.deleteRepo, { ...action.payload });
+    const response = yield call(repoSettingsService.getSettings, { owner, name });
+    yield put(fetchRepoSettings.success(response));
+  } catch (error) {
+    yield put(fetchRepoSettings.request(error.message));
+  }
 }
 
 function* watchDeleteRepo() {
-    yield takeEvery(types.DELETE_REPO, deleteRepo);
+  yield takeEvery(types.DELETE_REPO, deleteRepo);
 }
 
 export default function* repoSettingsSagas() {
-    yield all([watchSettingsRequest(), watchRenameRepo(), watchDeleteRepo()]);
+  yield all([watchSettingsRequest(), watchRenameRepo(), watchDeleteRepo()]);
 }
