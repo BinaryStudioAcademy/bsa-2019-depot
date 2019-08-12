07e3a0bd2b00f1e12577fe4577f48cfcad89b988
Michael Fridrikhson <fridrihson.1701@gmail.com>
2019-08-12 12:18:11 +0300

    fix: Ran eslint on server and client

--- client/src/containers/Profile/sagas.js
+++ client/src/containers/Profile/sagas.js
@@ -4,69 +4,69 @@ import * as signupService from '../../services/signupService';
 import { authorizeUser, fetchCurrentUser, signupRoutine } from '../../routines/routines';
 
 function* authorizationRequest({ payload }) {
-    try {
-        yield put(authorizeUser.request());
+  try {
+    yield put(authorizeUser.request());
 
-        const response = yield call(authService.login, payload);
-        const userData = yield apply(response, response.json);
+    const response = yield call(authService.login, payload);
+    const userData = yield apply(response, response.json);
 
-        if (response.status !== 200) {
-            throw new Error(response.message);
-        }
-
-        yield apply(localStorage, localStorage.setItem, ['token', userData.token]);
-        yield put(authorizeUser.success(userData.user));
-    } catch (error) {
-        yield put(authorizeUser.failure(error.message));
-    } finally {
-        yield put(authorizeUser.fulfill());
+    if (response.status !== 200) {
+      throw new Error(response.message);
     }
+
+    yield apply(localStorage, localStorage.setItem, ['token', userData.token]);
+    yield put(authorizeUser.success(userData.user));
+  } catch (error) {
+    yield put(authorizeUser.failure(error.message));
+  } finally {
+    yield put(authorizeUser.fulfill());
+  }
 }
 
 function* watchAuthorizationRequest() {
-    yield takeEvery(authorizeUser.TRIGGER, authorizationRequest);
+  yield takeEvery(authorizeUser.TRIGGER, authorizationRequest);
 }
 
 function* currentUserRequest() {
-    try {
-        yield put(fetchCurrentUser.request());
+  try {
+    yield put(fetchCurrentUser.request());
 
-        const response = yield call(authService.getCurrentUser);
+    const response = yield call(authService.getCurrentUser);
 
-        yield put(fetchCurrentUser.success(response));
-    } catch (error) {
-        yield put(fetchCurrentUser.failure(error.message));
-    } finally {
-        yield put(fetchCurrentUser.fulfill());
-    }
+    yield put(fetchCurrentUser.success(response));
+  } catch (error) {
+    yield put(fetchCurrentUser.failure(error.message));
+  } finally {
+    yield put(fetchCurrentUser.fulfill());
+  }
 }
 
 function* watchCurrentUserRequest() {
-    yield takeEvery(fetchCurrentUser.TRIGGER, currentUserRequest);
+  yield takeEvery(fetchCurrentUser.TRIGGER, currentUserRequest);
 }
 
 function* signup({ payload: { user, history } }) {
-    try {
-        yield put(signupRoutine.request());
+  try {
+    yield put(signupRoutine.request());
 
-        const response = yield call(signupService.signup, user);
-        const { token } = response;
+    const response = yield call(signupService.signup, user);
+    const { token } = response;
 
-        yield call(signupService.setToken, token);
-        yield put(signupRoutine.success(response.user));
+    yield call(signupService.setToken, token);
+    yield put(signupRoutine.success(response.user));
 
-        yield call(history.push, '/');
-    } catch (error) {
-        yield put(signupRoutine.failure(error.message));
-    } finally {
-        yield put(signupRoutine.fulfill());
-    }
+    yield call(history.push, '/');
+  } catch (error) {
+    yield put(signupRoutine.failure(error.message));
+  } finally {
+    yield put(signupRoutine.fulfill());
+  }
 }
 
 function* watchSignupRequest() {
-    yield takeEvery(signupRoutine.TRIGGER, signup);
+  yield takeEvery(signupRoutine.TRIGGER, signup);
 }
 
 export default function* profileSagas() {
-    yield all([watchAuthorizationRequest(), watchCurrentUserRequest(), watchSignupRequest()]);
+  yield all([watchAuthorizationRequest(), watchCurrentUserRequest(), watchSignupRequest()]);
 }
