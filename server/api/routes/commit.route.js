30e0c0b6b49282f3963b35d4128662a406cf4333
Andrey Akimov <rojer9999@gmail.com>
2019-08-12 16:34:13 +0300

    fix: Add catch block into commit route

--- server/api/routes/commit.route.js
+++ server/api/routes/commit.route.js
@@ -4,8 +4,10 @@ const { getCommits } = require('../services/commit.service');
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  getCommits({ ...req.body }).then(data => res.send(data));
+router.get('/', (req, res, next) => {
+  getCommits({ ...req.body })
+    .then(data => res.send(data))
+    .catch(next);
 });
 
 module.exports = router;
