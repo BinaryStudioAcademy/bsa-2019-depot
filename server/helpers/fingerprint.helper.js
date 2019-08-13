7fea98afeadf895ecc0e66acc0a730d048a04824
Michael Fridrikhson <fridrihson.1701@gmail.com>
2019-08-13 12:45:22 +0300

    fix: Fixed encoding for readFile

--- server/helpers/fingerprint.helper.js
+++ server/helpers/fingerprint.helper.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const fingerprintGenerator = require('ssh-fingerprint');
 
 const generateFingerprint = async () => new Promise((resolve, reject) => {
-  fs.readFile(process.env.PUBLIC_KEY_PATH, 'utf-8', (error, data) => {
+  fs.readFile(process.env.PUBLIC_KEY_PATH, 'utf8', (error, data) => {
     if (error) {
       reject(error);
     } else {
