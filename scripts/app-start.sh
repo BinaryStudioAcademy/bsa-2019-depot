a337f878e00923868a7938a7aafeb1f2b58bccb6
Alik <rakhmonov.alibek@binary-studio.com>
2019-08-13 13:37:22 +0300

    update commands

--- scripts/app-start.sh
+++ scripts/app-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pm2 delete Depot
+pm2 delete all
 cd /home/ubuntu/bsa-2019-depot/server/
 npx sequelize-cli db:migrate
-pm2 start server.js --name "Depot"
+pm2 start server.js --name Depot
