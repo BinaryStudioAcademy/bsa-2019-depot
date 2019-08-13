a605ce432409c80b3974bdb26daa61d27f0c042f
Alex Hlushenok <hlushenok.oleksandr@gmail.com>
2019-08-14 01:28:44 +0300

    feat: Add repository table into DB

---
+++ server/data/repositories/repository.repository.js
@@ -0,0 +1,13 @@
+const BaseRepository = require('./base.repository');
+const { RepositoryModel } = require('../models/index');
+
+class RepositoryRepository extends BaseRepository {
+  getByUser(userId) {
+    return this.model.findAll({ where: { userId } });
+  }
+  addRepository({ ...repositoryData }) {
+    return this.create(repositoryData);
+  }
+}
+
+module.exports = new RepositoryRepository(RepositoryModel);
