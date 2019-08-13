721fd61b86c194e381ff51fa2aa3a598a29aef09
Michael Fridrikhson <fridrihson.1701@gmail.com>
2019-08-13 14:42:41 +0300

    fix: Fixed branch service routes

--- client/src/scenes/CodeTab/sagas.js
+++ client/src/scenes/CodeTab/sagas.js
@@ -3,41 +3,41 @@ import * as branchesService from '../../services/branchesService';
 import { fetchLastCommitOnBranch, fetchFileTree } from '../../routines/routines';
 
 function* lastCommitRequest({ payload: { owner, repoName, branch } }) {
-    try {
-        yield put(fetchLastCommitOnBranch.request());
+  try {
+    yield put(fetchLastCommitOnBranch.request());
 
-        const response = yield call(branchesService.getLastCommit, owner, repoName, branch);
+    const response = yield call(branchesService.getLastCommit, owner, repoName, branch);
 
-        yield put(fetchLastCommitOnBranch.success(response));
-    } catch (error) {
-        yield put(fetchLastCommitOnBranch.failure(error.message));
-    } finally {
-        yield put(fetchLastCommitOnBranch.fulfill());
-    }
+    yield put(fetchLastCommitOnBranch.success(response));
+  } catch (error) {
+    yield put(fetchLastCommitOnBranch.failure(error.message));
+  } finally {
+    yield put(fetchLastCommitOnBranch.fulfill());
+  }
 }
 
 function* watchLastCommitRequest() {
-    yield takeEvery(fetchLastCommitOnBranch.TRIGGER, lastCommitRequest);
+  yield takeEvery(fetchLastCommitOnBranch.TRIGGER, lastCommitRequest);
 }
 
 function* fileTreeRequest({ payload: { owner, repoName, branch } }) {
-    try {
-        yield put(fetchFileTree.request());
+  try {
+    yield put(fetchFileTree.request());
 
-        const response = yield call(branchesService.getFileTree, owner, repoName, branch);
+    const response = yield call(branchesService.getFileTree, owner, repoName, branch);
 
-        yield put(fetchFileTree.success(response));
-    } catch (error) {
-        yield put(fetchFileTree.failure(error.message));
-    } finally {
-        yield put(fetchFileTree.fulfill());
-    }
+    yield put(fetchFileTree.success(response));
+  } catch (error) {
+    yield put(fetchFileTree.failure(error.message));
+  } finally {
+    yield put(fetchFileTree.fulfill());
+  }
 }
 
 function* watchFileTreeRequest() {
-    yield takeEvery(fetchFileTree.TRIGGER, fileTreeRequest);
+  yield takeEvery(fetchFileTree.TRIGGER, fileTreeRequest);
 }
 
 export default function* codeTabSagas() {
-    yield all([watchLastCommitRequest(), watchFileTreeRequest()]);
+  yield all([watchLastCommitRequest(), watchFileTreeRequest()]);
 }
