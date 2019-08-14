5b8e943442a43d0cbc931f7884466177bf68ee8d
olyakolesnichenko <kolesnichenko.zp@gmail.com>
2019-08-14 21:44:10 +0300

    fix: Add grid for main container

--- client/src/containers/Routing/index.jsx
+++ client/src/containers/Routing/index.jsx
@@ -21,6 +21,8 @@ import RepositoryPage from '../../scenes/Repository';
 import Header from '../Header';
 import Footer from '../../components/Footer';
 
+import './styles.module.scss';
+
 class Routing extends React.Component {
   componentDidMount() {
     this.props.fetchCurrentUser();
@@ -32,22 +34,24 @@ class Routing extends React.Component {
     return loading ? (
       <Spinner />
     ) : (
-      <>
-        <Header />
-        <Switch>
-          <Route exact path="/registration" component={Signup} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/" component={MainPage} />
-          <Route exact path="/forgot" component={Forgot} />
-          <Route exact path="/reset/:hash" component={Reset} />
-          <PrivateRoute exact path="/new" component={CreateRepository} />
-          <PrivateRoute path="/:username" component={Dashboard} />
-          <PrivateRoute exact path="/settings/profile" component={SettingsProfile} />
-          <PrivateRoute path="/:username/:reponame" component={RepositoryPage} />
-          <Route exact path="*" component={NotFound} />
-        </Switch>
+      <section className="main-wrapper">
+        <div className="content">
+          <Header />
+          <Switch>
+            <Route exact path="/registration" component={Signup} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/" component={MainPage} />
+            <Route exact path="/forgot" component={Forgot} />
+            <Route exact path="/reset/:hash" component={Reset} />
+            <PrivateRoute exact path="/new" component={CreateRepository} />
+            <PrivateRoute path="/:username" component={Dashboard} />
+            <PrivateRoute exact path="/settings/profile" component={SettingsProfile} />
+            <PrivateRoute path="/:username/:reponame" component={RepositoryPage} />
+            <Route exact path="*" component={NotFound} />
+          </Switch>
+        </div>
         <Footer />
-      </>
+      </section>
     );
   }
 }
