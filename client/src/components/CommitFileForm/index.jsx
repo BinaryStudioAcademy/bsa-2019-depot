00ee46d94ad61c88bc0dd9274bf79a104c7ab565
Michael Fridrikhson <fridrihson.1701@gmail.com>
2019-08-14 13:37:07 +0300

    feat: Fixed layout for commit form

--- client/src/components/CommitFileForm/index.jsx
+++ client/src/components/CommitFileForm/index.jsx
@@ -1,39 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Formik } from 'formik';
-import { Form, Radio } from 'semantic-ui-react';
-/*import { getUserImgLink } from '../../helpers/imageHelper';*/
+import { Form, Image, Segment, Radio } from 'semantic-ui-react';
+import { getUserImgLink } from '../../helpers/imageHelper';
+
+import styles from './styles.module.scss';
 
 const CommitFileForm = ({ avatar, initialBranch, handleSubmit }) => {
-  /*const [displayInput, setDisplayInput] = useState(false);
-  const [newBranch, setNewBranch] = useState('');*/
-
   return (
-    <Formik initialValues={{ message: '', branch: initialBranch }} onSubmit={handleSubmit}>
-      {({ values: { message, branch }, handleChange }) => (
-        <Form>
-          <Form.Input name="message" value={message} placeholder="Enter commit message" fluid />
-          <Form.Field>
-            <Radio
-              label={`Commit directly to the ${initialBranch} branch.`}
-              name="branch"
-              value={initialBranch}
-              checked={branch === initialBranch}
-              onChange={handleChange}
-            />
-          </Form.Field>
-          <Form.Field>
-            <Radio
-              label="Create a new branch for this commit."
-              name="branch"
-              /*value={newBranch}*/
-              checked={branch !== initialBranch}
-              onChange={handleChange}
-            />
-          </Form.Field>
-        </Form>
-      )}
-    </Formik>
+    <div className={styles.commitFormContainer}>
+      <Image src={getUserImgLink(avatar)} avatar />
+      <Formik initialValues={{ message: '', branch: initialBranch }} onSubmit={handleSubmit}>
+        {({ values: { message, branch }, handleChange }) => (
+          <Segment>
+            <Form>
+              <Form.Input name="message" value={message} placeholder="Enter commit message" fluid />
+              <Form.Field>
+                <Radio
+                  label={`Commit directly to the ${initialBranch} branch.`}
+                  name="branch"
+                  id="initial-branch"
+                  value={initialBranch}
+                  onChange={handleChange}
+                />
+              </Form.Field>
+              <Form.Field>
+                <Radio
+                  label="Create a new branch for this commit."
+                  name="branch"
+                  id="new-branch"
+                  value={branch}
+                  onChange={handleChange}
+                />
+                {branch !== initialBranch && (
+                  <Form.Input
+                    icon="code branch"
+                    iconPosition="left"
+                    name="branch"
+                    value={branch}
+                    width={4}
+                    className={styles.branchInput}
+                    placeholder="Enter new branch name"
+                    onChange={handleChange}
+                  />
+                )}
+              </Form.Field>
+            </Form>
+          </Segment>
+        )}
+      </Formik>
+    </div>
   );
 };
 
