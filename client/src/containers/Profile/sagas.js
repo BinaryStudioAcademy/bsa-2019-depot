57198201bbfb4e332c8935f063fad466225d6ba0
Olga Gnatenko <olgagnat13@gmail.com>
2019-08-15 12:37:23 +0300

    fix: Redirect to :username after successful setUsername

--- client/src/containers/Profile/sagas.js
+++ client/src/containers/Profile/sagas.js
@@ -93,7 +93,7 @@ function* setUsername({ payload: { username, user, history } }) {
     const { status } = response;
     if (status) {
       yield put(setUsernameRoutine.success(username));
-      yield call(history.push, '/dashboard');
+      yield call(history.push, '/:username');
     } else {
       yield put(setUsernameRoutine.failure(response.errorMessage));
     }
