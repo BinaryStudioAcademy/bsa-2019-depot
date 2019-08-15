6808220df91723ffcfed64cd66b9051a2c2f1572
Olga Gnatenko <olgagnat13@gmail.com>
2019-08-15 16:00:46 +0300

    fix: Change redirect to new approach

--- client/src/containers/Profile/sagas.js
+++ client/src/containers/Profile/sagas.js
@@ -93,7 +93,7 @@ function* setUsername({ payload: { username, user, history } }) {
     const { status } = response;
     if (status) {
       yield put(setUsernameRoutine.success(username));
-      yield call(history.push, `/${username}/dashboard`);
+      yield call(history.push, `/${username}?tab=repositories`);
     } else {
       yield put(setUsernameRoutine.failure(response.errorMessage));
     }
