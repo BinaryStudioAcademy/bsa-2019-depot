bad43c8cff0368b0c6fac0fec769da7399cc95ce
Alex Hlushenok <hlushenok.oleksandr@gmail.com>
2019-08-16 13:33:13 +0300

    fix: Fixed error message for username validation

--- client/src/scenes/Login/index.jsx
+++ client/src/scenes/Login/index.jsx
@@ -33,7 +33,7 @@ const validationSchema = Yup.object().shape({
 
 const usernameValidationSchema = Yup.object().shape({
   username: Yup.string()
-    .required('Username address is required!')
+    .required('Username is required!')
     .matches(/^(([a-zA-Z0-9]+-)*[a-zA-Z0-9]+){1,39}$/)
 });
 
