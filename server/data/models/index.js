c2675b1d0fafe475e65735961cd6ecac100ec9e5
Vasyl Boyko <vboiko0@gmail.com>
2019-08-17 15:04:40 +0300

    feat: Add models and associations

--- server/data/models/index.js
+++ server/data/models/index.js
@@ -4,15 +4,21 @@ const associate = require('../db/associations');
 const User = orm.import('./user');
 const SshKey = orm.import('./sshkey');
 const Repository = orm.import('./repository');
+const OrgUser = orm.import('./orguser');
+const Role = orm.import('./role');
 
 associate({
   User,
   SshKey,
-  Repository
+  Repository,
+  OrgUser,
+  Role
 });
 
 module.exports = {
   UserModel: User,
   SshKeyModel: SshKey,
-  RepositoryModel: Repository
+  RepositoryModel: Repository,
+  OrgUserModel: OrgUser,
+  RoleModel: Role
 };
