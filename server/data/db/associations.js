1a87adbd78ce8e5a27848ccd86713b9b74422836
denterekhov <den.n.terekhov@gmail.com>
2019-08-17 22:17:52 +0300

    feat: Add issueComment to associations

--- server/data/db/associations.js
+++ server/data/db/associations.js
@@ -1,15 +1,19 @@
 module.exports = (models) => {
-  const { User, SshKey, Repository, Issue } = models;
+  const { User, SshKey, Repository, Issue, IssueComment } = models;
 
   SshKey.belongsTo(User);
 
   User.hasMany(SshKey);
   User.hasMany(Repository);
   User.hasMany(Issue);
+  User.hasMany(IssueComment);
   Repository.hasMany(Issue);
+  Issue.hasMany(IssueComment);
 
   Repository.belongsTo(User);
   Issue.belongsTo(User);
   Issue.belongsTo(Repository);
+  IssueComment.belongsTo(User);
+  IssueComment.belongsTo(Issue);
   // Repository.hasOne(DefaultBranch);
 };
