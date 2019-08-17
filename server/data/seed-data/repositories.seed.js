456c3c575aadcff60c7e1ebefa1c42bcc135572d
Nick <niiickolay@gmail.com>
2019-08-17 23:46:13 +0300

    feat: Add repositories seed

--- server/data/seed-data/repositories.seed.js
+++ server/data/seed-data/repositories.seed.js
@@ -1,12 +1,14 @@
-const getName = Math.random()
+const getName = () => Math.random()
   .toString(36)
   .replace(/[^a-z]+/g, '')
-  .substr(0, 5);
+  .substr(0, 10);
+
 const now = new Date();
-const repositoriesSeed = new Array(50).fill({
+
+const repositoriesSeed = new Array(50).fill(true).map(() => ({
   name: getName(),
   createdAt: now,
   updatedAt: now
-});
+}));
 
 module.exports = repositoriesSeed;
