4564a378b5fcdfc3ec5781bf32c32d533bcd1340
Nick <niiickolay@gmail.com>
2019-08-18 20:53:39 +0300

    refactor: Change repositories-data fetching from server

--- client/src/services/repositoryService.js
+++ client/src/services/repositoryService.js
@@ -3,8 +3,7 @@ import callWebApi from '../helpers/webApiHelper';
 export const getRepositories = async (owner, filter) => {
   const response = await callWebApi({
     endpoint: `/api/repo/${owner}/repos`,
-    type: 'GET',
-    body: filter
+    type: 'GET'
   });
   return response.json();
 };
