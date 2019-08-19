5512c17370ee72b9154442b750255d2d2ca2b5c3
Andrew Minikh <andreoven@gmail.com>
2019-08-19 17:31:11 +0300

    fix: Update token helper

--- server/helpers/token.helper.js
+++ server/helpers/token.helper.js
@@ -1,14 +1,14 @@
 const jwt = require('jsonwebtoken');
 const { secret, expiresIn } = require('../config/jwt.config');
 
-const verifyToken = async (token) => {
-  await jwt.verify(token, secret, (err, authorizedData) => {
+const verifyToken = token => new Promise((resolve, reject) => {
+  jwt.verify(token, secret, (err, authorizedData) => {
     if (err) {
-      return err;
+      reject(err);
     }
-    return authorizedData;
+    resolve(authorizedData);
   });
-};
+});
 
 module.exports = {
   createToken: data => jwt.sign(data, secret, { expiresIn }),
