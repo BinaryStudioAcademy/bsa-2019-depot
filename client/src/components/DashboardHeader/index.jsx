0539375247ecaaeb6e9ebea7634159fdb51ff84b
Nick <niiickolay@gmail.com>
2019-08-20 10:23:20 +0300

    fix: highlight active tab

--- client/src/components/DashboardHeader/index.jsx
+++ client/src/components/DashboardHeader/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import styles from './styles.module.scss';
@@ -10,43 +10,54 @@ const DashboardHeader = ({
   starsCount,
   followersCount,
   followingCount,
-  baseUrl
-}) => (
-  <nav className={styles.navbar}>
-    <ul className={styles.navbar_list}>
-      <li>
-        <NavLink to={baseUrl} className={styles.link} activeClassName="selected">
-          Overview
-        </NavLink>
-      </li>
-      <li>
-        <NavLink className={styles.link} to={`${baseUrl}?tab=repositories`} activeClassName="selected">
-          Repositories{repoCount && <span>{repoCount}</span>}
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to={`${baseUrl}?tab=projects`} className={styles.link} activeClassName="selected">
-          Projects{projectsCount && <span>{projectsCount}</span>}
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to={`${baseUrl}?tab=stars`} className={styles.link} activeClassName="selected">
-          Stars{starsCount && <span>{starsCount}</span>}
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to={`${baseUrl}?tab=followers`} className={styles.link} activeClassName="selected">
-          Followers{followersCount && <span>{followersCount}</span>}
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to={`${baseUrl}?tab=following`} className={styles.link} activeClassName="selected">
-          Following{followingCount && <span>{followingCount}</span>}
-        </NavLink>
-      </li>
-    </ul>
-  </nav>
-);
+  baseUrl,
+  activeTab
+}) => {
+  const getClassName = (tab, activeTab) => {
+    if (!activeTab && tab === 'overview') {
+      return 'dashboard_link selected';
+    }
+
+    return tab === activeTab ? 'dashboard_link selected' : 'dashboard_link';
+  };
+
+  return (
+    <nav className={styles.navbar}>
+      <ul className={styles.navbar_list}>
+        <li>
+          <Link to={baseUrl} className={getClassName('overview', activeTab)}>
+            Overview
+          </Link>
+        </li>
+        <li>
+          <Link to={`${baseUrl}?tab=repositories`} className={getClassName('repositories', activeTab)}>
+            Repositories{repoCount && <span>{repoCount}</span>}
+          </Link>
+        </li>
+        <li>
+          <Link to={`${baseUrl}?tab=projects`} className={getClassName('projects', activeTab)}>
+            Projects{projectsCount && <span>{projectsCount}</span>}
+          </Link>
+        </li>
+        <li>
+          <Link to={`${baseUrl}?tab=stars`} className={getClassName('stars', activeTab)}>
+            Stars{starsCount && <span>{starsCount}</span>}
+          </Link>
+        </li>
+        <li>
+          <Link to={`${baseUrl}?tab=followers`} className={getClassName('followers', activeTab)}>
+            Followers{followersCount && <span>{followersCount}</span>}
+          </Link>
+        </li>
+        <li>
+          <Link to={`${baseUrl}?tab=following`} className={getClassName('followers', activeTab)}>
+            Following{followingCount && <span>{followingCount}</span>}
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+};
 
 DashboardHeader.propTypes = {
   repoCount: PropTypes.array,
@@ -54,7 +65,8 @@ DashboardHeader.propTypes = {
   starsCount: PropTypes.number,
   followersCount: PropTypes.number,
   followingCount: PropTypes.number,
-  baseUrl: PropTypes.string
+  baseUrl: PropTypes.string,
+  activeTab: PropTypes.string
 };
 
 export default DashboardHeader;
