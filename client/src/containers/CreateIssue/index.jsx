c5515f191b8d3a173e5fba6f44ed56db39bcde33
Andrew Minikh <andreoven@gmail.com>
2019-08-20 17:26:16 +0300

    fix: Add issue validation

--- client/src/containers/CreateIssue/index.jsx
+++ client/src/containers/CreateIssue/index.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactMde from 'react-mde';
 import ReactMarkdown from 'react-markdown';
 import 'react-mde/lib/styles/css/react-mde-all.css';
+import * as Yup from 'yup';
 
-import { Container, Grid, Form, Button, Dropdown, Popup, Image } from 'semantic-ui-react';
+import { Container, Grid, Button, Dropdown, Form,  Popup, Image } from 'semantic-ui-react';
+import { Formik,  Field } from 'formik';
 import styles from './styles.module.scss';
+import { InputError } from '../../components/InputError';
 
 // labels mock
 const labelOptions = [
@@ -21,27 +24,24 @@ const assigneeOptions = [
   { key: 'dz', value: 'dz', text: 'Michael Algol' }
 ];
 
+const validationSchema = Yup.object().shape({
+  issueTitle: Yup.string()
+    .max(256, 'Maximum length - 255 characters')
+});
+
 class CreateIssuePage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       title: '',
       body: '',
-      selectedTab: 'write',
-      isDisabled: true
+      selectedTab: 'write'
     };
 
-    this.onTitleChange = this.onTitleChange.bind(this);
     this.onBodyChange = this.onBodyChange.bind(this);
     this.renderPreview = this.renderPreview.bind(this);
     this.onTabChange = this.onTabChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
-  }
-
-  onTitleChange(evt) {
-    const title = evt.target.value;
-    const isDisabled = !title;
-    this.setState({ ...this.state, title, isDisabled });
   }
 
   onBodyChange(body) {
@@ -64,59 +64,64 @@ class CreateIssuePage extends React.Component {
   });
 
   render() {
-    const { title, body, selectedTab, isDisabled } = this.state;
+    const { body, selectedTab } = this.state;
     return (
       <Container>
-        <Form className={styles.issueForm} onSubmit={this.onSubmit}>
-          <Grid>
-            <Grid.Column>
-              <Popup
-                content={'current user'}
-                trigger={<Image avatar src="https://react.semantic-ui.com/images/wireframe/image.png" />}
-              />
-            </Grid.Column>
-            <Grid.Column width={10}>
-              <Form.Input value={title} onChange={this.onTitleChange} placeholder={'Title'} />
-              <div className={styles.commentEditor}>
-                <ReactMde
-                  value={body}
-                  onChange={this.onBodyChange}
-                  selectedTab={selectedTab}
-                  onTabChange={this.onTabChange}
-                  generateMarkdownPreview={this.renderPreview}
-                />
-              </div>
-              <Button color="green" floated="right" type="submit" disabled={isDisabled}>
-                Submit new issue
-              </Button>
-            </Grid.Column>
-            <Grid.Column width={5}>
-              <div className={styles.sidebar}>
-                <div className={styles.sidebarItem}>
-                  <Dropdown
-                    multiple
-                    selection
-                    fluid
-                    options={labelOptions}
-                    placeholder="Choose an option"
-                    renderLabel={this.renderLabel}
+        <Formik  onSubmit={this.onSubmit} validationSchema={validationSchema}>
+          {({ errors,  handleChange, issueTitle }) => (
+            <Form className={styles.issueForm} onSubmit={this.onSubmit}>
+              <Grid>
+                <Grid.Column>
+                  <Popup
+                    content={'current user'}
+                    trigger={<Image avatar src="https://react.semantic-ui.com/images/wireframe/image.png" />}
                   />
-                </div>
-                <div className={styles.sidebarItem}>
-                  <Dropdown
-                    clearable
-                    fluid
-                    multiple
-                    search
-                    selection
-                    options={assigneeOptions}
-                    placeholder="Assignees"
-                  />
-                </div>
-              </div>
-            </Grid.Column>
-          </Grid>
-        </Form>
+                </Grid.Column>
+                <Grid.Column width={10}>
+                  <Field name="issueTitle" value={issueTitle} onChange={handleChange} placeholder="Title" className={styles.titleInput} />
+                  <InputError name="issueTitle" />
+                  <div className={styles.commentEditor}>
+                    <ReactMde
+                      value={body}
+                      onChange={this.onBodyChange}
+                      selectedTab={selectedTab}
+                      onTabChange={this.onTabChange}
+                      generateMarkdownPreview={this.renderPreview}
+                    />
+                  </div>
+                  <Button color="green" floated="right" type="submit" disabled={errors.issueTitle}>
+                    Submit new issue
+                  </Button>
+                </Grid.Column>
+                <Grid.Column width={5}>
+                  <div className={styles.sidebar}>
+                    <div className={styles.sidebarItem}>
+                      <Dropdown
+                        multiple
+                        selection
+                        fluid
+                        options={labelOptions}
+                        placeholder="Choose an option"
+                        renderLabel={this.renderLabel}
+                      />
+                    </div>
+                    <div className={styles.sidebarItem}>
+                      <Dropdown
+                        clearable
+                        fluid
+                        multiple
+                        search
+                        selection
+                        options={assigneeOptions}
+                        placeholder="Assignees"
+                      />
+                    </div>
+                  </div>
+                </Grid.Column>
+              </Grid>
+            </Form>
+          )}
+        </Formik>
       </Container>
     );
   }
