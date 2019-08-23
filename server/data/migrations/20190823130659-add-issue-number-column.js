0fa94f639983b4557f2efdc484b00ff18d1ebd25
denterekhov <den.n.terekhov@gmail.com>
2019-08-23 22:26:26 +0300

    fix: Split migration file into two

--- server/data/migrations/20190823130659-add-issue-number-column.js
+++ server/data/migrations/20190823130659-add-issue-number-column.js
@@ -3,14 +3,9 @@ module.exports = {
     queryInterface.addColumn('issues', 'number', {
       allowNull: false,
       type: Sequelize.INTEGER
-    }, { transaction }),
-    queryInterface.addConstraint('issues', ['number', 'repositoryId'], {
-      type: 'unique',
-      name: 'unique_number_per_repo'
     }, { transaction })
   ])),
   down: queryInterface => queryInterface.sequelize.transaction(transaction => Promise.all([
-    queryInterface.removeConstraint('issues', 'unique_number_per_repo', { transaction }),
     queryInterface.removeColumn('issues', 'number', { transaction })
   ]))
 };
