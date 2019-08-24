d700334459f0701188358871380505d8665de684
Alex Hlushenok <hlushenok.oleksandr@gmail.com>
2019-08-24 03:37:43 +0300

    new: changed user preview in organization dashboard

--- client/src/components/UserPreview/index.jsx
+++ client/src/components/UserPreview/index.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import { getOrganizationImgLink } from '../../helpers/imageHelper';
+import { getUserImgLink } from '../../helpers/imageHelper';
 import PropTypes from 'prop-types';
 
 import styles from './styles.module.scss';
 
 const UserPreview = ({ size, orgUser }) => (
-  <a href={`${orgUser.username}`} className={styles.orgUserItem}>
-    <img
-      alt="organization logo"
-      src={getOrganizationImgLink(orgUser.imgUrl)}
-      className={`orgUserPreview-${size || ''}`}
-    />
+  <a href={`/${orgUser.username}`} className={styles.orgUserItem}>
+    <img alt="organization logo" src={getUserImgLink(orgUser.imgUrl)} className={`orgUserPreview-${size || ''}`} />
   </a>
 );
 
