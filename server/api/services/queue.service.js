a628f6697b649b2106cac12a17e71fc4dda85ea8
Michael Fridrikhson <fridrihson.1701@gmail.com>
2019-08-27 02:17:53 +0300

    feat: Moved syncDb function to the helper to use it when creating commits via UI

--- server/api/services/queue.service.js
+++ server/api/services/queue.service.js
@@ -1,40 +1,9 @@
 const amqp = require('amqplib/callback_api');
 const { connectionUrl, emailQueue, repoDataQueue } = require('../../config/rabbitmq.config');
 const { sendEmail } = require('../../helpers/email.helper');
-const userRepository = require('../../data/repositories/user.repository');
-const repoRepository = require('../../data/repositories/repository.repository');
-const branchRepository = require('../../data/repositories/branch.repository');
-const commitRepository = require('../../data/repositories/commit.repository');
+const { syncDb } = require('../../helpers/repo.helper');
 
 let ch = null;
-
-const syncDb = async (commits, branch) => {
-  const { id: userId } = await userRepository.getByUsername(commits[0].repoOwner);
-  const { id: repositoryId } = await repoRepository.getByUserAndReponame(userId, commits[0].repoName);
-  const commitAuthors = await Promise.all(commits.map(({ userEmail }) => userRepository.getByEmail(userEmail)));
-  const addedCommits = await Promise.all(commitAuthors.map(({ id: authorId }, index) => commitRepository.add({
-    sha: commits[index].sha,
-    message: commits[index].message,
-    userId: authorId,
-    createdAt: new Date(commits[index].createdAt),
-    repositoryId
-  })));
-  const headCommitId = addedCommits.find(({ sha }) => sha === branch.newHeadSha).id;
-
-  const existingBranch = await branchRepository.getByNameAndRepoId(branch.name, repositoryId);
-  if (!existingBranch) {
-    await branchRepository.create({
-      name: branch.name,
-      headCommitId,
-      createdAt: addedCommits[addedCommits.length - 1].createdAt,
-      repositoryId
-    });
-  } else {
-    await branchRepository.updateById(existingBranch.id, {
-      headCommitId
-    });
-  }
-};
 
 amqp.connect(connectionUrl, (err, connection) => {
   if (err) {
