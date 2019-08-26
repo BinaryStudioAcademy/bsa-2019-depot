24cbc51be37b5c1e5f0e60112ea3f4d77b20736d
Nick <niiickolay@gmail.com>
2019-08-26 23:28:02 +0300

    refactor: Connect IssuePage & RepositorySettings to RepositoryPage

--- client/src/scenes/RepositoryPage/index.jsx
+++ client/src/scenes/RepositoryPage/index.jsx
@@ -3,9 +3,14 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Switch, Route } from 'react-router-dom';
 import RepositoryHeader from '../../components/RepositoryHeader';
+import RepositoryTab from '../../containers/RepositoryTab';
 import IssuesTab from '../../containers/IssuesTab/index';
+import IssueComments from '../../components/IssueComments/index';
+import CreateIssuePage from '../../containers/CreateIssuePage';
 import RepoSettings from '../../containers/SettingsTab/index';
+import PrivateTab from '../../containers/PrivateTab';
 import { fetchCurrentRepo } from '../../routines/routines';
+import { Container } from 'semantic-ui-react';
 
 class RepositoryPage extends React.Component {
   async componentDidMount() {
@@ -28,7 +33,24 @@ class RepositoryPage extends React.Component {
     const { username, reponame } = match.params;
 
     return (
-      <div></div>
+      <>
+        <RepositoryHeader
+          owner={username}
+          repoName={reponame}
+          issueCount={0}
+          activePage={pathname.split('/')[3]}
+          baseUrl={match.url}
+        />
+        <Container>
+          <Switch>
+            <Route exact path={`${match.path}`} component={RepositoryTab} />
+            <Route exact path={`${match.path}/issues`} component={IssuesTab} />
+            <Route exact path={`${match.path}/issues/new`} component={CreateIssuePage} />
+            <Route exact path={`${match.path}/issues/:number`} component={IssueComments} />
+            <PrivateTab exact path={`${match.path}/settings`} component={RepoSettings} />
+          </Switch>
+        </Container>
+      </>
     )
   }
 }
