af97963ce89f3f89a18984f6e86813fa3cf27f2b
Aleksandr Kuharenko <sandrk27@gmail.com>
2019-08-27 23:36:09 +0300

    feat: Add color config

---
+++ mobile/app/config/color.config.js
@@ -0,0 +1,11 @@
+const colors = {
+  blueButton: '#2e53db',
+  greyText: '#737373',
+  greyButton: '#f0f0f0',
+  greyBorder: '#BEBEBE',
+  white: '#fff',
+  black: '#000',
+  blueLink: '#4f60ac'
+};
+
+export default colors;
