e25fa9071175eddf0eaf9597321ec95f1b7771c8
Alex Hlushenok <hlushenok.oleksandr@gmail.com>
2019-08-27 19:13:51 +0300

    new: Type column in repository table

---
+++ server/data/migrations/20190827154758-add-repository-type-column.js
@@ -0,0 +1,15 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.sequelize.transaction(transaction => Promise.all([
+    queryInterface.addColumn(
+      'repositories',
+      'isPublic',
+      {
+        allowNull: false,
+        type: Sequelize.BOOLEAN
+      },
+      { transaction }
+    )
+  ])),
+
+  down: queryInterface => queryInterface.sequelize.transaction(transaction => Promise.all([queryInterface.removeColumn('issues', 'number', { transaction })]))
+};
