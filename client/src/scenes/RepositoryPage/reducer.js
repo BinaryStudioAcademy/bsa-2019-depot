a7d704bea59078e7cd1f999510196a57a66765ac
Nick <niiickolay@gmail.com>
2019-08-27 14:13:49 +0300

    refactor: change initial state of RepositoryPage

--- client/src/scenes/RepositoryPage/reducer.js
+++ client/src/scenes/RepositoryPage/reducer.js
@@ -2,42 +2,56 @@ import { fetchCurrentRepo } from '../../routines/routines';
 
 const initialCurrentRepoState = {
   currentRepoInfo: {
-    originalRepo: {
-      name: '',
-      user: {
-        username: ''
-      }
+    "id": "",
+    "name": "",
+    "owner": {
+      "id": "",
+      "username": ""
+    },
+    "description": "",
+    "website": "",
+    "createdAt": "",
+    "updatedAt": "",
+    "starsCount": null,
+    "forksCount": null,
+    "issuesCount": null,
+    "branches": [],
+    "defaultBranch": "master",
+    "originalRepo": {
+      "id": "",
+      "name": "",
+      "owner": ""
     }
   },
-  loading: false,
+  loading: true,
   error: null
 };
 
 export const currentRepoReducer = (state = initialCurrentRepoState, action) => {
   switch (action.type) {
-  case fetchCurrentRepo.TRIGGER:
-    return {
-      ...state,
-      loading: true
-    };
-  case fetchCurrentRepo.SUCCESS:
-    return {
-      ...state,
-      currentRepoInfo: {
-        ...action.payload
-      }
-    };
-  case fetchCurrentRepo.FAILURE:
-    return {
-      ...state,
-      error: action.payload
-    };
-  case fetchCurrentRepo.FULFILL:
-    return {
-      ...state,
-      loading: false
-    };
-  default:
-    return state;
+    case fetchCurrentRepo.TRIGGER:
+      return {
+        ...state,
+        loading: true
+      };
+    case fetchCurrentRepo.SUCCESS:
+      return {
+        ...state,
+        currentRepoInfo: {
+          ...action.payload
+        }
+      };
+    case fetchCurrentRepo.FAILURE:
+      return {
+        ...state,
+        error: action.payload
+      };
+    case fetchCurrentRepo.FULFILL:
+      return {
+        ...state,
+        loading: false
+      };
+    default:
+      return state;
   }
 };
