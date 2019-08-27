b2c2d2690e85a0179c45e358abeb6a3c09edcce2
Nick <niiickolay@gmail.com>
2019-08-27 16:47:56 +0300

    feat: Add branchReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -4,13 +4,18 @@ import issuesReducer from '../containers/IssuesTab/reducer';
 import { forgotReducer as forgot } from '../scenes/Forgot/reducer';
 import { resetReducer as reset } from '../scenes/Reset/reducer';
 import { commitsData } from '../containers/CommitsPage/reducer';
-import { lastCommitReducer, fileTreeReducer, newFileReducer } from '../scenes/CodeTab/reducer';
+import { branchReducer, newFileReducer } from '../scenes/CodeTab/reducer';
 import { repoSettingsData } from '../containers/SettingsTab/reducer';
 // import { currentRepoReducer } from '../containers/RepositoryTab/reducer';
 import profile from '../containers/Profile/reducer';
 import { branchesData } from '../containers/BranchesTab/reducer';
 import createOrg from '../scenes/CreateOrganization/reducer';
 import { currentRepoReducer } from '../scenes/RepositoryPage/reducer';
+
+const currentRepo = combineReducers({
+  repository: currentRepoReducer,
+  branch: branchReducer
+});
 
 export default combineReducers({
   profile,
@@ -19,10 +24,8 @@ export default combineReducers({
   issuesData: issuesReducer,
   commitsData,
   branchesData,
-  lastCommitData: lastCommitReducer,
-  fileTreeData: fileTreeReducer,
   newFile: newFileReducer,
   repoSettingsData,
-  currentRepo: currentRepoReducer,
+  currentRepo,
   createOrg
 });
--- client/src/scenes/CodeTab/sagas.js
+++ client/src/scenes/CodeTab/sagas.js
@@ -1,51 +1,14 @@
 import { takeEvery, put, call, all } from 'redux-saga/effects';
 import * as branchesService from '../../services/branchesService';
-import { fetchLastCommitOnBranch, fetchFileTree } from '../../routines/routines';
 import { fetchBranch } from '../../routines/routines';
 
-function* lastCommitRequest({ payload: { username, reponame, branch } }) {
-  try {
-    yield put(fetchLastCommitOnBranch.request());
-
-    const response = yield call(branchesService.getLastCommit, username, reponame, branch);
-
-    yield put(fetchLastCommitOnBranch.success(response));
-  } catch (error) {
-    yield put(fetchLastCommitOnBranch.failure(error.message));
-  } finally {
-    yield put(fetchLastCommitOnBranch.fulfill());
-  }
-}
-
-function* watchLastCommitRequest() {
-  yield takeEvery(fetchLastCommitOnBranch.TRIGGER, lastCommitRequest);
-}
-
-function* fileTreeRequest({ payload: { username, reponame, branch, query } }) {
-  try {
-    yield put(fetchFileTree.request());
-
-    const response = yield call(branchesService.getFileTree, username, reponame, branch, query);
-
-    yield put(fetchFileTree.success(response));
-  } catch (error) {
-    yield put(fetchFileTree.failure(error.message));
-  } finally {
-    yield put(fetchFileTree.fulfill());
-  }
-}
-
-function* watchFileTreeRequest() {
-  yield takeEvery(fetchFileTree.TRIGGER, fileTreeRequest);
-}
-
-function* branchRequest({ payload: { repoId, branch } }) {
+function* branchRequest({ payload: { repoID, branch } }) {
   try {
     yield put(fetchBranch.request());
-    const response = yield call(branchesService.getBranch, repoId, branch);
+    const response = yield call(branchesService.getBranch, repoID, branch);
     yield put(fetchBranch.success(response));
   } catch (error) {
-    yield put(fetchFileTree.failure(error.message));
+    yield put(fetchBranch.failure(error.message));
   }
 }
 
@@ -54,5 +17,5 @@ function* watchBranchRequest() {
 }
 
 export default function* codeTabSagas() {
-  yield all([watchLastCommitRequest(), watchFileTreeRequest(), watchBranchRequest()]);
+  yield all([watchBranchRequest()]);
 }
