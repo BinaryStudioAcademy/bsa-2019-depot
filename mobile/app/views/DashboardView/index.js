b53a22b597ada8c9d671915edf2c85be4bf8142e
Aleksandr Kuharenko <sandrk27@gmail.com>
2019-08-28 22:59:06 +0300

    feat: App temporary dashboard view

---
+++ mobile/app/views/DashboardView/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import { fetchCurrentUser } from '../../routines/routines';
+import storageHelper from '../../helpers/storageHelper';
+
+class DashboardView extends React.Component {
+  logOut = async () => {
+    const { navigation, fetchCurrentUser } = this.props;
+    storageHelper.clear();
+    await fetchCurrentUser();
+    navigation.navigate('Auth');
+  };
+
+  async componentDidMount() {
+    const { isAuthorized, navigation } = this.props;
+    if (!isAuthorized) navigation.navigate('Auth');
+  }
+
+  render() {
+    const { username, email } = this.props.currentUser;
+    return (
+      <View>
+        <Text>{username}</Text>
+        <Text>{email}</Text>
+        <TouchableOpacity onPress={this.logOut}>
+          <Text>{'Log out'}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+DashboardView.propTypes = {
+  navigation: PropTypes.object,
+  fetchCurrentUser: PropTypes.func,
+  isAuthorized: PropTypes.bool,
+  currentUser: PropTypes.object
+};
+
+const mapStateToProps = ({ profile: { currentUser, isAuthorized } }) => ({
+  currentUser,
+  isAuthorized
+});
+
+const mapDispatchToProps = { fetchCurrentUser };
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DashboardView);
