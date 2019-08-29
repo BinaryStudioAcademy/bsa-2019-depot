de0154b6678db2d6c69d000d9e41a41acc890377
Aleksandr Kuharenko <sandrk27@gmail.com>
2019-08-30 01:10:43 +0300

    feat: Add ReposList container

---
+++ mobile/app/containers/ReposList/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+//import colors from '../../config/color.config';
+
+export default StyleSheet.create({
+  emptyText: {
+    alignSelf: 'center',
+    fontSize: 16
+  },
+  form: {
+    width: '100%'
+  },
+  headerText: {
+    fontSize: 22
+  }
+});
