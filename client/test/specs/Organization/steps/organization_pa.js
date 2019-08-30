565fdbe8080ad42d83d8ec073b15b4064d6b7248
olyakolesnichenko <kolesnichenko.zp@gmail.com>
2019-08-30 13:13:27 +0300

    fix: Organizations list

--- client/test/specs/Organization/steps/organization_pa.js
+++ client/test/specs/Organization/steps/organization_pa.js
@@ -1,47 +1,46 @@
 const orgPage = require('../page/organization_po');
 
 class OrgSteps {
+  enternameofneworg(value) {
+    orgPage.orgnameField.waitForExist(5000);
+    orgPage.orgnameField.clearValue();
+    orgPage.orgnameField.setValue(value);
+  }
 
-    enternameofneworg(value) {
-        orgPage.orgnameField.waitForExist(5000);
-        orgPage.orgnameField.clearValue();
-        orgPage.orgnameField.setValue(value);
-    }
+  enteremailofneworg(value) {
+    orgPage.orgemailField.waitForExist(5000);
+    orgPage.orgemailField.clearValue();
+    orgPage.orgemailField.setValue(value);
+  }
 
-    enteremailofneworg(value) {
-        orgPage.orgemailField.waitForExist(5000);
-        orgPage.orgemailField.clearValue();
-        orgPage.orgemailField.setValue(value);
-    }
+  saveneworgbutton() {
+    orgPage.savebutton.waitForEnabled(5000);
+    orgPage.savebutton.click();
+  }
 
-    saveneworgbutton() {
-        orgPage.savebutton.waitForEnabled(5000);
-        orgPage.savebutton.click();
-    }
+  createdNewOrgName() {
+    orgPage.createdNewOrgName.getText();
+  }
 
-    createdNewOrgName() {
-        orgPage.createdNewOrgName.getText();
-    }
+  newrepofororgbutton() {
+    orgPage.newrepofororgbutton.waitForEnabled(5000);
+    orgPage.newrepofororgbutton.click();
+  }
 
-    newrepofororgbutton() {
-        orgPage.newrepofororgbutton.waitForEnabled(5000);
-        orgPage.newrepofororgbutton.click();
-    }
+  enterereponame(value) {
+    orgPage.orgrepoField.waitForExist(5000);
+    orgPage.orgrepoField.clearValue();
+    orgPage.orgrepoField.setValue(value);
+  }
 
-    enterereponame(value) {
-        orgPage.orgrepoField.waitForExist(5000);
-        orgPage.orgrepoField.clearValue();
-        orgPage.orgrepoField.setValue(value);
-    }
+  createrepofororgbutton() {
+    orgPage.createrepofororgbutton.waitForEnabled(5000);
+    orgPage.createrepofororgbutton.click();
+  }
 
-    createrepofororgbutton() {
-        orgPage.createrepofororgbutton.waitForEnabled(5000);
-        orgPage.createrepofororgbutton.click();
-    }
-
-    createdRepoofOrg() {
-        orgPage.createdRepoofOrg.getText();
-    }
+  createdRepoofOrg() {
+    orgPage.createdRepoofOrg.getText();
+  }
 }
 
 module.exports = new OrgSteps();
