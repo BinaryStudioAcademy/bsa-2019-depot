9bc47ae4f8f36e8ee68ab1735e9b49470d33fa28
Nick <niiickolay@gmail.com>
2019-09-05 10:10:48 +0300

    refactor: Change data format for fetching pinned repos

--- server/data/repositories/pinned-repository.repository.js
+++ server/data/repositories/pinned-repository.repository.js
@@ -1,9 +1,35 @@
+const sequelize = require('../db/connection');
 const BaseRepository = require('./base.repository');
-const { PinnedRepositoryModel } = require('../models/index');
+const { PinnedRepositoryModel, RepositoryModel, UserModel } = require('../models/index');
 
 class PinnedRepository extends BaseRepository {
-  getPinnedRepositories(userId) {
-    return this.model.findAll({ where: { userId } });
+  getAll(userId) {
+    return this.model.findAll({
+      where: { userId },
+      attributes: ['id'],
+      include: [
+        {
+          model: RepositoryModel,
+          where: { deletedAt: null },
+          attributes: {
+            include: [
+              [
+                sequelize.literal(`
+                (SELECT COUNT(*)
+                FROM "stars"
+                WHERE "repository"."id" = "stars"."repositoryId"
+                AND "stars"."deletedAt" IS NULL)`),
+                'starsCount'
+              ]
+            ]
+          },
+          include: {
+            model: UserModel,
+            attributes: ['username']
+          }
+        }
+      ]
+    });
   }
 
   deleteByUserId(userId) {
