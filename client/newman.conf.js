e453f8395895fd89bc2f351cc51e4eb8a2bd78ba
Andrii_Karun <andriy.karun@binary-studio.com>
2019-09-05 16:42:20 +0300

    test: delete unneeded logins; added insecure var

--- client/newman.conf.js
+++ client/newman.conf.js
@@ -1,12 +1,19 @@
 const newman = require('newman');
 
+const { DEV } = process.env;
+
 newman.run(
   {
     collection: require('./test/api/DepotPostam.postman_collection.json'),
     globals: require('./test/api/GlobalEnvVar.postman_globals.json'),
     reporters: ['cli', 'json'],
     reporter: { json: { export: './test/api/output/jsonResults.json' } },
-    delayRequest: 500
+    delayRequest: 500,
+    insecure: DEV || false
   },
-  process.exit
+  err => {
+    if (err) {
+      throw err;
+    }
+  }
 );
