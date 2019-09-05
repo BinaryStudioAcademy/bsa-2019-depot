4fb4fc572a61366c28d17f1a7694ad57920b80ac
Nick <niiickolay@gmail.com>
2019-09-05 11:22:55 +0300

    Revert "feat: Update pinned-repos.service for getting last updated repos"
    This reverts commit 829fdbed59b32ae0041c4746fa5b62ebc570d107.

--- server/api/services/pinned-repos.service.js
+++ server/api/services/pinned-repos.service.js
@@ -1,24 +1,12 @@
 const pinnedReposRepository = require('../../data/repositories/pinned-repository.repository');
-const repositoryRepository = require('../../data/repositories/repository.repository');
 
 const getPinnedRepos = async userId => pinnedReposRepository.getAll(userId);
-
-const REPOS_LIMIT = 6;
-const IS_OWNER = true;
 
 const setPinnedRepos = async (userId, repositories) => {
   try {
     await pinnedReposRepository.deleteByUserId(userId);
-
-    if (!repositories.length) {
-      const repos = await repositoryRepository.getByUserWithOptions(userId, IS_OWNER, { limit: REPOS_LIMIT });
-      return repos;
-    }
-
     const promises = repositories.map(repositoryId => pinnedReposRepository.create({ userId, repositoryId }));
-    const pinnedRepos = await Promise.all(promises).then(array => array.map(item => item.get({ plain: true })));
-
-    return pinnedRepos;
+    return Promise.all(promises);
   } catch (error) {
     throw error;
   }
