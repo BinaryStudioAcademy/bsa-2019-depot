0c9109b441b169d79373f910737ab72e14be237b
Depot Test <depot.demo.user@gmail.com>
2019-09-07 19:53:25 +0300

    feat: Check that issue and label are from same repo

--- server/api/services/issue-label.service.js
+++ server/api/services/issue-label.service.js
@@ -1,4 +1,6 @@
 const IssueLabelRepository = require('../../data/repositories/issue-label.repository');
+const IssueRepository = require('../../data/repositories/issue.repository');
+const LabelRepository = require('../../data/repositories/label.repository');
 const CustomError = require('../../helpers/error.helper');
 
 const getIssueLabelById = async (id) => {
@@ -6,7 +8,24 @@ const getIssueLabelById = async (id) => {
   return issueLabel || Promise.reject(new CustomError(404, `Issue label with id ${id} not found`));
 };
 
-const addIssueLabel = issueLabelData => IssueLabelRepository.addIssueLabel(issueLabelData);
+const addIssueLabel = async (issueLabelData) => {
+  const { issueId, labelId } = issueLabelData;
+  const issue = await IssueRepository.getById(issueId);
+  if (!issue) {
+    // return 400 in this method since this request is used only by API and is not an actual route
+    return Promise.reject(new CustomError(400, `Issue ${issueId} does not exist`));
+  }
+  const label = await LabelRepository.getById(labelId);
+  if (!label) {
+    return Promise.reject(new CustomError(400, `Label ${labelId} does not exist`));
+  }
+  if (label.repositoryId !== issue.repositoryId) {
+    return Promise.reject(
+      new CustomError(400, `Issue ${issueId} and label ${labelId} do not belong to the same repository`)
+    );
+  }
+  return IssueLabelRepository.addIssueLabel(issueLabelData);
+};
 
 const deleteIssueLabelById = id => IssueLabelRepository.deleteIssueLabelById(id);
 
