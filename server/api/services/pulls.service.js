9a70cd18b0841e472b79eb35aec14a3fbb61e3c6
Michael Fridrikhson <fridrihson.1701@gmail.com>
2019-09-07 20:19:07 +0300

    feat: Added PR review statuses for list of PRs

--- server/api/services/pulls.service.js
+++ server/api/services/pulls.service.js
@@ -7,6 +7,7 @@ const userRepository = require('../../data/repositories/user.repository');
 const pullRepository = require('../../data/repositories/pull-request.repository');
 const prStatusRepository = require('../../data/repositories/pr-status.repository');
 const pullLabelRepository = require('../../data/repositories/pull-label.repository');
+const pullReviewerRepository = require('../../data/repositories/pull-reviewer.repository');
 const repoHelper = require('../../helpers/repo.helper');
 const CustomError = require('../../helpers/error.helper');
 
@@ -171,7 +172,32 @@ const getRepoByPullId = pullId => pullRepository.getRepoByPullId(pullId);
 const getRepoPulls = async (repositoryId, sort, authorId, title, isOpened) => {
   const status = await prStatusRepository.getByName('OPEN');
   const { id: statusId } = status.get({ plain: true });
-  return pullRepository.getPulls(repositoryId, sort, authorId, title, isOpened, statusId);
+  const pullsObjects = await Promise.all(await pullRepository.getPulls(repositoryId, sort, authorId, title, isOpened, statusId));
+  const pulls = pullsObjects.map(pull => pull.get({ plain: true }));
+
+  const pullsReviews = await Promise.all(pulls.map(({ id }) => pullReviewerRepository.getReviewersForPull(id)));
+
+  const statuses = pullsReviews
+    .map(((reviewObjs) => {
+      if (!reviewObjs.length) {
+        return '';
+      }
+
+      const reviews = reviewObjs.map(review => review.get({ plain: true }));
+
+      reviews.sort(({ updatedAt: updatedAtA }, { updatedAt: updatedAtB }) => new Date(updatedAtB) - new Date(updatedAtA));
+      const approvedId = reviews.findIndex(review => review.status.name === 'APPROVED');
+      const changesRequestedId = reviews.findIndex(review => review.status.name === 'CHANGES REQUESTED');
+      if (approvedId === -1 && changesRequestedId === -1) {
+        return 'Review required';
+      }
+      if (approvedId > changesRequestedId) {
+        return 'Approved';
+      }
+      return 'Requested changes';
+    }));
+
+  return pulls.map((pull, index) => ({ ...pull, reviewStatus: statuses[index] }));
 };
 
 const getPullCount = async (repositoryId, isOpened) => {
