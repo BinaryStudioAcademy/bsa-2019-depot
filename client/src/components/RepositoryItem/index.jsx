ae97c4b2bd9633e2fad566f30dbf02063bf5e28e
Michael Fridrikhson <fridrihson.1701@gmail.com>
2019-09-09 22:23:19 +0300

    refactor: Removed excessive logical check

--- client/src/components/RepositoryItem/index.jsx
+++ client/src/components/RepositoryItem/index.jsx
@@ -125,17 +125,18 @@ class RepositoryItem extends React.Component {
 
   render() {
     const {
+      repo,
       repo: { name, isStar, isPublic, defaultBranch },
       username,
       type,
     } = this.props;
+    console.log(repo);
     const { updatedAt, isEmpty, commitsPerYear } = this.state;
     const starsCount = Number(this.props.repo.starsCount);
     const repoType = isPublic ? '' : 'Private';
     const language = defaultBranch
     && defaultBranch.languageStats
     && defaultBranch.languageStats.length
-    && defaultBranch.languageStats[0].language
       ? defaultBranch.languageStats[0].language
       : null;
 
